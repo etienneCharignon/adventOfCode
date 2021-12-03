47bd1e9ab6d4ac9c430b8675a3716d617e5c1311
Étienne Charignon <etienne.charignon@gmail.com>
2021-12-03 10:03:34 +0100

    jour 3 partie 2

--- j3.py
+++ j3.py
@@ -2,20 +2,22 @@ from inputj3 import input
 
 diag = input.split('\n')
 
-counters = []
-for i in range(len(diag[0])):
-    counters.append([0, 0])
 
-for binary in diag:
-    for j, bit in enumerate(binary):
-        print(j)
-        if(bit == "0"):
-            counters[j][0] += 1
-        else:
-            counters[j][1] += 1
+def counters(diag):
+    counters = []
+    for i in range(len(diag[0])):
+        counters.append([0, 0])
+
+    for binary in diag:
+        for j, bit in enumerate(binary):
+            if(bit == "0"):
+                counters[j][0] += 1
+            else:
+                counters[j][1] += 1
+    return counters
 
 
-print(counters)
+print(counters(diag))
 
 
 def mapGamma(counter):
@@ -26,14 +28,32 @@ def mapGamma(counter):
 
 
 def mapEpsylon(counter):
-    if counter[0] < counter[1]:
+    if counter[0] <= counter[1]:
         return "0"
     else:
         return "1"
 
 
-gamma = int("".join(map(mapGamma, counters)), 2)
-epsylon = int("".join(map(mapEpsylon, counters)), 2)
+gamma = int("".join(map(mapGamma, counters(diag))), 2)
+epsylon = int("".join(map(mapEpsylon, counters(diag))), 2)
 print(gamma)
 print(epsylon)
 print(gamma*epsylon)
+
+
+print("Partie 2")
+
+
+def search(binaries, i, criteria):
+    if(len(binaries) == 1):
+        return binaries[0]
+
+    mostCommon = criteria(counters(binaries)[i])
+    selectedBinaries = [b for b in binaries if b[i] == mostCommon]
+    return search(selectedBinaries, i+1, criteria)
+
+
+oxygen = int(search(diag, 0, mapGamma), 2)
+co2 = int(search(diag, 0, mapEpsylon), 2)
+
+print(oxygen * co2)
