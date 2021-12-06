10c75598accd666df87e04b5ce70cd9e618812e9
Étienne Charignon <etienne.charignon@gmail.com>
2021-12-06 10:25:56 +0100

    Nettoyage

--- 2021/j6_lanternfish.py
+++ 2021/j6_lanternfish.py
@@ -2,36 +2,25 @@ def init_world(input):
     world = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
 
     def add(point, diagram):
-        if(point in diagram):
-            diagram[point] += 1
-        else:
-            diagram[point] = 1
+        diagram[point] += 1
 
     inputs = input.split(',')
     for fish in inputs:
-        add(int(fish), world)
+        world[int(fish)] += 1
 
     return world
 
 
 def next_generation(world):
-    new_fish = []
+    new_world = {}
+    new_world[0] = world[1]
+    new_world[1] = world[2]
+    new_world[2] = world[3]
+    new_world[3] = world[4]
+    new_world[4] = world[5]
+    new_world[5] = world[6]
+    new_world[6] = world[0] + world[7]
+    new_world[7] = world[8]
+    new_world[8] = world[0]
 
-    def decrease_day(fish):
-        if(fish == 0):
-            new_fish.append(8)
-            return 6
-        return fish - 1
-
-    result = {}
-    result[0] = world[1]
-    result[1] = world[2]
-    result[2] = world[3]
-    result[3] = world[4]
-    result[4] = world[5]
-    result[5] = world[6]
-    result[6] = world[0] + world[7]
-    result[7] = world[8]
-    result[8] = world[0]
-
-    return result
+    return new_world
