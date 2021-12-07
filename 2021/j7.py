9edf737755790a1800f0c2969787916afb0b1270
Étienne Charignon <etienne.charignon@gmail.com>
2021-12-07 08:33:28 +0100

    j7 partie 2

--- 2021/j7.py
+++ 2021/j7.py
@@ -3,24 +3,34 @@ from inputj7 import position
 
 
 def calculate_median(positions):
-    positions = sorted(positions)
     numberOfCrabs = len(positions)
     if numberOfCrabs < 1:
         return None
     if numberOfCrabs % 2 == 0:
-        print("paire")
         return int((positions[int((numberOfCrabs-1)/2)] + positions[int((numberOfCrabs+1)/2)]) / 2.0)
     else:
-        print("impaire")
         return positions[int((numberOfCrabs-1)/2)]
 
 
+def calcul_fuel(destination):
+    def fuel(position):
+        return sum(range(1, abs(position - destination) + 1))
+
+    fuel = (sum(map(fuel, positions)))
+    return fuel
+
+
 positions = list(map(int, position.split(',')))
+positions = sorted(positions)
 median = calculate_median(positions)
 
-
-def distance(position):
-    return abs(position - median)
-
-
-print(sum(map(distance, positions)))
+min_fuel = calcul_fuel(0)
+print(min_fuel)
+for target in range(1, len(positions)):
+    fuel = calcul_fuel(target)
+    print(fuel)
+    if(fuel < min_fuel):
+        min_fuel = fuel
+    else:
+        print(min_fuel)
+        break
