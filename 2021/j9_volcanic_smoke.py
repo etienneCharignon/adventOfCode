8496fa0133418c9e77329692babdc06073998834
Étienne Charignon <etienne.charignon@gmail.com>
2021-12-09 13:52:39 +0100

    j9 partie 2

--- 2021/j9_volcanic_smoke.py
+++ 2021/j9_volcanic_smoke.py
@@ -25,3 +25,57 @@ def find_lower_point(world):
             if(isLower):
                 low_point += current + 1
     return low_point
+
+
+def is_lower(point, world):
+    line, col = point
+    current = world[line][col]
+    isLower = True
+    if(col > 0):
+        isLower = current < world[line][col - 1]
+    if(col < len(world[line]) - 1):
+        isLower = isLower and current < world[line][col + 1]
+    if(line > 0):
+        isLower = isLower and current < world[line - 1][col]
+    if(line < len(world) - 1):
+        isLower = isLower and current < world[line + 1][col]
+    return isLower
+
+
+def find_basins(world):
+    basins = {}
+    for line in range(0, len(world)):
+        for col in range(0, len(world[line])):
+            if(is_lower((line, col), world)):
+                basins[(line, col)] = 1
+    return basins
+
+
+def find_lowest(world, point):
+    line, col = point
+    current = world[line][col]
+    if(col > 0):
+        left = world[line][col - 1]
+        if(left < current):
+            return find_lowest(world, (line, col - 1))
+    if(col < len(world[line]) - 1):
+        right = world[line][col + 1]
+        if(right < current):
+            return find_lowest(world, (line, col + 1))
+    if(line > 0):
+        up = world[line - 1][col]
+        if(up < current):
+            return find_lowest(world, (line - 1, col))
+    if(line < len(world) - 1):
+        down = world[line + 1][col]
+        if(down < current):
+            return find_lowest(world, (line + 1, col))
+    return point
+
+
+def find_wich_basin(world, current, basins):
+    l, c = current
+    if(is_lower(current, world) or world[l][c] == 9):
+        return basins
+    basins[find_lowest(world, current)] += 1
+    return basins
--- 2021/test_j9_volcanic_smoke.py
+++ 2021/test_j9_volcanic_smoke.py
@@ -1,6 +1,7 @@
+import numpy
 from samplej9 import input
 from inputj9 import rinput
-from j9_volcanic_smoke import find_lower_point, parse_input
+from j9_volcanic_smoke import find_lower_point, parse_input, find_basins, find_wich_basin
 
 
 def test_find_lower_point():
@@ -17,3 +18,42 @@ def test_parse_input():
 def test_count_lower_point():
     assert find_lower_point(parse_input(input)) == 15
     assert find_lower_point(parse_input(rinput)) == 550
+
+
+def test_find_basins():
+    assert find_basins(parse_input(input)) == {
+        (0, 1): 1,
+        (0, 9): 1,
+        (2, 2): 1,
+        (4, 6): 1
+    }
+
+
+def test_find_wich_basin():
+    assert find_wich_basin([[1, 0, 1]], (0, 2), {(0, 1): 1}) == {(0, 1): 2}
+    assert find_wich_basin([[1, 0, 9]], (0, 2), {(0, 1): 1}) == {(0, 1): 1}
+
+
+def test_find_all_basins():
+    world = parse_input(input)
+    basins = find_basins(world)
+    for line in range(0, len(world)):
+        for col in range(0, len(world[line])):
+            basins = find_wich_basin(world, (line, col), basins)
+    assert basins == {(0, 1): 3, (0, 9): 9, (2, 2): 14, (4, 6): 9}
+
+    basins_sizes = list(reversed(sorted(basins.values())))
+    tree_biggest = [basins_sizes[index] for index in [0, 1, 2]]
+    assert numpy.prod(tree_biggest) == 1134
+
+
+def test_find_all_rbasins():
+    world = parse_input(rinput)
+    basins = find_basins(world)
+    for line in range(0, len(world)):
+        for col in range(0, len(world[line])):
+            basins = find_wich_basin(world, (line, col), basins)
+
+    basins_sizes = list(reversed(sorted(basins.values())))
+    tree_biggest = [basins_sizes[index] for index in [0, 1, 2]]
+    assert numpy.prod(tree_biggest) == 1100682
