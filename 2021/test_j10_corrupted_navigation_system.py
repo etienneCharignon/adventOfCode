af958dfd8515ca5e97787a61640d1b1e88af6cd8
Étienne Charignon <etienne.charignon@gmail.com>
2021-12-10 09:23:39 +0100

    j10 partie 2

--- 2021/test_j10_corrupted_navigation_system.py
+++ 2021/test_j10_corrupted_navigation_system.py
@@ -1,11 +1,19 @@
 from samplej10 import input
 from inputj10 import rinput
+from j7 import calculate_median
 
 SCORE = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137
+}
+
+SCORE_P2 = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
 }
 
 
@@ -37,6 +45,36 @@ def total_score(input):
         cleared = remove_oppening(remove_good_pair(line))
         score += score_corruption(cleared)
     return score
+
+
+def complete(line):
+    line = line[::-1]
+    for pair in [('(', ')'), ('{', '}'), ('[', ']'), ('<', '>')]:
+        signe, replacement = pair
+        if(signe in line):
+            line = line.replace(signe, replacement)
+    return line
+
+
+def test_completion():
+    assert complete('[({([[{{') == '}}]])})]'
+
+
+def score_completion(completion):
+    score = 0
+    for char in completion:
+        score = score * 5 + SCORE_P2[char]
+    return score
+
+
+def test_complete_lines():
+    score = []
+    for line in input.split('\n'):
+        cleared = remove_good_pair(line)
+        corrupted = remove_oppening(cleared)
+        if(len(corrupted) == 0):
+            score.append(score_completion(complete(cleared)))
+    assert calculate_median(sorted(score)) == 288957
 
 
 def test_find_corruption():
