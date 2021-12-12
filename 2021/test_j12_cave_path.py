2f94c320fbb9cce112504249db3e6ddd14ef9522
Étienne Charignon <etienne.charignon@gmail.com>
2021-12-12 11:53:23 +0100

    j12 partie 2

--- 2021/test_j12_cave_path.py
+++ 2021/test_j12_cave_path.py
@@ -1,14 +1,18 @@
 from inputj12 import sample1, sample2, input
 
 
+def is_authorised_visit(visited):
+    small_visited_caves = [cave for cave in visited if cave.islower()]
+    return (len(small_visited_caves) - len(set(small_visited_caves))) < 2
+
+
 def find_paths(caves, connexions, path_to_there):
     paths = []
-    print(caves)
     for cave in caves:
+        new_path_to_there = path_to_there + [cave]
         if(cave not in connexions):
             paths.append([cave])
-        elif(cave.isupper() or cave not in path_to_there):
-            new_path_to_there = path_to_there + [cave]
+        elif(cave.isupper() or is_authorised_visit(new_path_to_there)):
             sub_paths = find_paths(connexions[cave], connexions, new_path_to_there)
             for sub_path in sub_paths:
                 sub_path.insert(0, cave)
@@ -19,7 +23,8 @@ def find_paths(caves, connexions, path_to_there):
 def count_path(connexions):
     paths = find_paths(['start'], connexions, [])
     paths = [path for path in paths if path[-1] == 'end']
-    print(paths)
+    # for path in paths:
+    #     print(path)
     return len(paths)
 
 
@@ -41,9 +46,9 @@ def as_map(input):
 
 
 def test_count_paths():
-    assert(count_path(as_map(sample1))) == 10
-    assert(count_path(as_map(sample2))) == 226
-    assert(count_path(as_map(input))) == 226
+    assert(count_path(as_map(sample1))) == 36
+    assert(count_path(as_map(sample2))) == 3509
+    assert(count_path(as_map(input))) == 147784
 
 
 def test_as_map():
