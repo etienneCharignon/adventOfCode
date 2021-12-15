28a3d1753f7c657ac4b58d48db0dc69130ca8e27
Étienne Charignon <etienne.charignon@gmail.com>
2021-12-15 20:07:08 +0100

    j15 deuxième essai tout pourri

--- 2021/test_j15_chiton.py
+++ 2021/test_j15_chiton.py
@@ -1,44 +1,45 @@
-from inputj15 import sample_input
+from inputj15 import sample_input, input
 
 
 def parse_input(input):
     cave = []
+    risks = []
     for row in input.split('\n'):
         cave.append(list(map(int, row)))
-    return cave
+        risks.append([100 for i in range(len(row))])
+    return cave, risks
+
+
+def print_it(field):
+    for row in field:
+        print(' '.join(map(str, row)))
 
 
 def test_parse_input():
-    assert parse_input(sample_input) == []
+    assert parse_input(sample_input)[0][0][0] == 1
 
 
-def is_min(actualMin, minx, miny, x, y, input, risk):
-    if(y == len(input) or x == len(input[0]) or y < 0 or x < 0):
-        return (actualMin, minx, miny)
+def compute_risk(risks, input, x, y, risk):
+    if(x < 0 or y < 0):
+        return risks
+    risks[y][x-1] = min(risk + input[y][x-1], risks[y][x-1])
+    risks = compute_risk(risks, input, x - 1, y, risks[y][x-1])
+    risks[y-1][x] = min(risk + input[y-1][x], risks[y-1][x])
+    risks = compute_risk(risks, input, x, y - 1, risks[y-1][x])
+    return risks
 
-    if(actualMin > risk + input[y][x]):
-        return (risk + input[y][x], x, y)
-    else:
-        return (actualMin, minx, miny)
 
-
-def find_lowset_risk(input, x, y, fromx, fromy, risk):
-    minimum = risk
-    if(y - 1 != fromy):
-        minimum, minx, miny = is_min(minimum, x, y, x, y - 1, input, risk)
-    if(x+1 != fromx):
-        minimum, minx, miny = is_min(minimum, minx, miny, x + 1, y, input, risk)
-    if(y+1 != fromy):
-        minimum, minx, miny = is_min(minimum, minx, miny, x, y + 1, input, risk)
-    if(x-1 != fromx):
-        minimum, minx, miny = is_min(minimum, minx, miny, x - 1, y, input, risk)
-    if(minx == 0 and miny == 0):
-        return minimum
-    else:
-        return find_lowset_risk(input, minx, miny, x, y, risk)
+def compute_lower_risk(input):
+    input, risks = parse_input(input)
+    x = len(input[0]) - 1
+    y = len(input) - 1
+    print(x)
+    print(y)
+    assert False
+    risks = compute_risk(risks, input, x, y, input[y][x])
+    print_it(risks)
+    return risks[0][0] - input[0][0]
 
 
 def test_find_lowest_risk():
-    x = len(sample_input[0]) - 1
-    y = len(sample_input) - 1
-    assert find_lowset_risk(sample_input, x, y, 0, 0, sample_input[y][x]) == 40
+    assert compute_lower_risk(input) == 40
