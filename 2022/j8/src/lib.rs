0e5d6768075494895c8149c60616dd54d0faf766
Étienne Charignon <etienne.charignon@gmail.com>
2022-12-08 10:49:13 +0100

    j8 part 2

--- 2022/j8/src/lib.rs
+++ 2022/j8/src/lib.rs
@@ -1,5 +1,7 @@
 mod inputs;
 
+use std::cmp;
+
 fn parse_input(input: &str) -> Vec<Vec<usize>> {
     let mut result = vec![];
     for lines in input.lines() {
@@ -61,6 +63,65 @@ fn count_visible(forest: Vec<Vec<usize>>) -> usize {
     visible
 }
 
+fn scope_left(forest: &Vec<Vec<usize>>, r: usize, c: usize) -> usize {
+    let mut scope = 0;
+    for i in (0..c).rev() {
+        scope += 1;
+        if forest[r][i] >= forest[r][c]{
+            break;
+        }
+    }
+    scope
+}
+
+fn scope_up(forest: &Vec<Vec<usize>>, r: usize, c: usize) -> usize {
+    let mut scope = 0;
+    for i in (0..r).rev() {
+        scope += 1;
+        if forest[i][c] >= forest[r][c]{
+            break;
+        }
+    }
+    scope
+}
+
+fn scope_down(forest: &Vec<Vec<usize>>, r: usize, c: usize) -> usize {
+    let mut scope = 0;
+    for i in r+1..forest.len() {
+        scope += 1;
+        if forest[i][c] >= forest[r][c]{
+            break;
+        }
+    }
+    scope
+}
+
+fn scope_right(forest: &Vec<Vec<usize>>, r: usize, c: usize) -> usize {
+    let mut scope = 0;
+    for i in c+1..forest[r].len() {
+        scope += 1;
+        if forest[r][i] >= forest[r][c]{
+            break;
+        }
+    }
+    scope
+}
+
+fn scope(forest: &Vec<Vec<usize>>, r: usize, c: usize) -> usize {
+    scope_up(forest, r, c) * scope_left(forest, r, c) * scope_down(forest, r, c) * scope_right(forest, r, c)
+}
+
+fn higest_scope(forest: Vec<Vec<usize>>) -> usize {
+    let mut higest: usize = 0;
+    let size: usize = forest.len() - 1;
+    for r in 1..size {
+        for c in 1..size  {
+            higest = cmp::max(higest, scope(&forest, r, c));
+        }
+    }
+    higest
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -116,5 +177,12 @@ mod tests {
         assert_eq!(count_visible(parse_input(inputs::EXAMPLE)), 21);
         assert_eq!(count_visible(parse_input(inputs::INPUT)), 1843);
     }
+    
+    #[test]
+    fn it_find_highest_scenic_score() {
+        assert_eq!(higest_scope(parse_input(inputs::EXAMPLE)), 8);
+        assert_eq!(higest_scope(parse_input(inputs::INPUT)), 180000);
+    }
+
 
 }
