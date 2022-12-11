3ee35f9748d9869ebea707900580a48eb4b8c556
Étienne Charignon <etienne.charignon@gmail.com>
2022-12-11 10:35:11 +0100

    j11 partie 1

--- 2022/j11/src/inputs.rs
+++ 2022/j11/src/inputs.rs
@@ -1,39 +1,56 @@
-struct Monkey<'a>
+#[allow(dead_code)]
+pub struct Monkey
 {
-    items: &'a mut Vec<usize>,
-    operation: &'a dyn Fn(usize) -> usize,
-    test: usize,
-    dest: (u8, u8)
+    pub test: usize,
+    pub dest: (usize, usize)
 }
 
-impl<'a> Monkey<'a>
-{
-    fn example() -> &'a mut Vec<Monkey<'a>> {
-        &mut vec![
-            Monkey {
-                items: &mut vec![79, 98],
-                operation: &|old| { old * 19 },
-                test: 23,
-                dest: (2, 3)
-            },
-            Monkey {
-                items: &mut vec![54, 65, 75, 74],
-                operation: &|old| { old + 6 },
-                test: 19,
-                dest: (2, 0)
-            },
-            Monkey {
-                items: &mut vec![79, 60, 97],
-                operation: &|old| { old * old },
-                test: 13,
-                dest: (1, 3)
-            },
-            Monkey {
-                items: &mut vec![74],
-                operation: &|old| { old + 3 },
-                test: 17,
-                dest: (0, 1)
-            },
-        ]
+#[allow(dead_code)]
+pub fn example() -> Vec<Monkey> {
+    vec![
+        Monkey { test: 23, dest: (2, 3) },
+        Monkey { test: 19, dest: (2, 0) },
+        Monkey { test: 13, dest: (1, 3) },
+        Monkey { test: 17, dest: (0, 1) },
+    ]
+}
+
+#[allow(dead_code)]
+pub fn input() -> Vec<Monkey> {
+    vec![
+        Monkey { test: 2, dest: (5, 2) },
+        Monkey { test: 13, dest: (4, 3) },
+        Monkey { test: 5, dest: (5, 1) },
+        Monkey { test: 3, dest: (6, 7) },
+        Monkey { test: 11, dest: (7, 3) },
+        Monkey { test: 17, dest: (4, 1) },
+        Monkey { test: 7, dest: (0, 2) },
+        Monkey { test: 19, dest: (6, 0) },
+    ]
+}
+
+#[allow(dead_code)]
+pub fn operate_example (monkey_number: usize, item: usize) -> usize {
+    match monkey_number {
+        0 => { item * 19 },
+        1 => { item + 6 },
+        2 => { item * item },
+        3 => { item + 3 },
+        _ => panic!("unknown monkey number")
+    }
+}
+
+#[allow(dead_code)]
+pub fn operate_input (monkey_number: usize, item: usize) -> usize {
+    match monkey_number {
+        0 => { item * 13 },
+        1 => { item + 7 },
+        2 => { item + 2 },
+        3 => { item * 2 },
+        4 => { item * item },
+        5 => { item + 6 },
+        6 => { item + 1 },
+        7 => { item + 8 },
+        _ => panic!("unknown monkey number")
     }
 }
