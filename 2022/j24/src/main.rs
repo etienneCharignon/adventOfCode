86804d6731c917e349adfedddad41c6f45f664e0
Étienne Charignon <etienne.charignon@gmail.com>
2022-12-28 19:33:13 +0100

    j24 partie 2

--- 2022/j24/src/main.rs
+++ 2022/j24/src/main.rs
@@ -1,9 +1,9 @@
 mod inputs;
 use j24::Position;
-use j24::find_path;
+use j24::solve;
 use j24::read_blizzards;
 
 fn main() {
-    let time = find_path(Position(1, 0, 0), Position(120, 25, 0), read_blizzards(inputs::INPUT));
-    println!("{}", time);
+    let time = solve(Position(1, 0, 0), Position(120, 26, 0), read_blizzards(inputs::INPUT));
+    println!("{:?}", time);
 }
