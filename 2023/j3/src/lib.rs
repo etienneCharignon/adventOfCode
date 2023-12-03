98ea19042cda3a5ca9f210fcf97deeda7756acdd
Étienne Charignon <etienne.charignon@gmail.com>
2023-12-03 15:33:29 +0100

    j3 partie 2
    Comme me l'a fait remarqué Paul il y avait plus simple.
    En constuisant une map avec en clé les coordonnées des étoiles

--- 2023/j3/src/lib.rs
+++ 2023/j3/src/lib.rs
@@ -5,6 +5,7 @@ use regex::Regex;
 
 lazy_static! {
     static ref RE: Regex = Regex::new(r"\d+").unwrap();
+    static ref STAR: Regex = Regex::new(r"[*]").unwrap();
 }
 
 pub fn is_symbol(c: u8) -> bool {
@@ -44,20 +45,15 @@ pub fn is_surounded(row_index: usize, from: usize, to: usize, world: &str) -> bo
     false
 }
 
-pub fn find_row_part(row_index: usize, row: &str, world: &str) -> Vec<u32> {
+pub fn find_row_part(row_index: usize, row: &str, world: &str) -> Vec<(usize, usize, usize, u32)> {
     RE.captures_iter(row).map(|c| {
         let m = c.get(0).unwrap();
-        if is_surounded(row_index, m.start(), m.end(), world) {
-            c[0].parse::<u32>().unwrap()
-        }
-        else {
-            0
-        }
+        (row_index, m.start(), m.end(), c[0].parse::<u32>().unwrap())
     }).collect()
 }
 
-pub fn find_parts(input: &str) -> Vec<u32> {
-    let mut parts: Vec<u32> = Vec::new();
+pub fn find_parts(input: &str) -> Vec<(usize, usize, usize, u32)> {
+    let mut parts: Vec<(usize, usize, usize, u32)> = Vec::new();
     let mut row_index: usize = 0;
     for row in input.lines() {
         parts.extend(find_row_part(row_index, row, input));
@@ -66,9 +62,51 @@ pub fn find_parts(input: &str) -> Vec<u32> {
     parts
 }
 
+pub fn find_ajacent(parts: &Vec<(usize, usize, usize, u32)>, x: usize, y: usize) -> Vec<u32> {
+    println!("{:?}", (x, y));
+    let adjacent: Vec<u32> = parts.iter().filter(|p| is_close_to(**p, x, y)).map(|p| p.3).collect();
+    println!("{:?}", adjacent);
+    if adjacent.iter().count() == 2 {
+        adjacent
+    }
+    else {
+        Vec::<u32>::from([0])
+    }
+}
+
+pub fn solve(input: &str) -> u32 {
+    let parts = find_parts(input);
+    let row_len = input.chars().position(|c| c == '\n').unwrap() + 1;
+    STAR.captures_iter(input).map(|c| {
+        let m = c.get(0).unwrap();
+        find_ajacent(&parts, m.start()/row_len, m.start()%row_len).iter().product::<u32>()
+    }).sum()
+}
+
+pub fn is_close_to(part: (usize, usize, usize, u32), r: usize, c: usize) -> bool {
+    let fx = if c > 0 { c - 1 } else { c };
+    let fy = if r > 0 { r - 1 } else { r };
+    for x in fx..=(c + 1) {
+        for y in fy..=(r + 1) {
+            if x >= part.1 && x < part.2 && y == part.0 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn it_is_close_to() {
+        assert_eq!(is_close_to((2, 2, 4, 123), 1, 3), true);
+        assert_eq!(is_close_to((0, 0, 3, 123), 1, 3), true);
+        assert_eq!(is_close_to((0, 0, 3, 123), 2, 3), false);
+    }
+
 
     #[test]
     fn it_is_symbol() {
@@ -93,12 +131,18 @@ mod tests {
     }
 
     #[test]
+    fn it_find_parts() {
+        assert_eq!(find_parts(".....\n.123."), vec![(1, 1, 4, 123)]);
+    }
+
+    #[test]
     fn it_solve_example() {
-        assert_eq!(find_parts(inputs::EXAMPLE).iter().sum::<u32>(), 4361);
+        assert_eq!(solve(inputs::EXAMPLE), 467835);
     }
 
     #[test]
     fn it_solve() {
-        assert_eq!(find_parts(inputs::INPUT).iter().sum::<u32>(), 521601);
+        // assert_eq!(find_parts(inputs::INPUT).iter().sum::<u32>(), 521601);
+        assert_eq!(solve(inputs::INPUT), 80694070);
     }
 }
