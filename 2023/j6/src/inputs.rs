1d21cf1efefd2b2373702a2ff7702a7b8da0b41d
Étienne Charignon <etienne.charignon@gmail.com>
2023-12-06 15:08:50 +0100

    jour 6, partie 1 et 2
    pas de programmation aujourd'hui, j'ai juste utilisé GéoGebra

---
+++ 2023/j6/src/inputs.rs
@@ -0,0 +1,4 @@
+pub fn example_times() -> Vec<u32> { vec![7, 15, 20] }
+pub fn example_distances() -> Vec<u32> { vec![9, 40, 200] }
+pub fn input_times() -> Vec<u32> { vec![62, 64, 91, 90] }
+pub fn input_distances() -> Vec<u32> { vec![553, 1010, 1473, 1074] }
