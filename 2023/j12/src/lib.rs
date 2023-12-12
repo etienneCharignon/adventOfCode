d983b558e7f149b200cdf93fbc9c99374c6aff93
Étienne Charignon <etienne.charignon@gmail.com>
2023-12-12 22:06:43 +0100

    Jour 12 partie 2
    J'ai surtout passer un temps de malade sur la partie 1, mais ensuite ,
    l'ajout de la map n'a pas été trop pénible (j'ai juste perdu du temps
    parce que je l'ai tout d'abord crue inefficace)

--- 2023/j12/src/lib.rs
+++ 2023/j12/src/lib.rs
@@ -2,9 +2,12 @@ mod inputs;
 
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashMap;
+
+
 
 lazy_static! {
-    static ref NUMBER: Regex = Regex::new(r"\d+").unwrap();
+    static ref RE: Regex = Regex::new(r"(\S+) (\S+)").unwrap();
 }
 
 /*
@@ -39,45 +42,66 @@ pub fn start_with(line: &[char], group: &String) -> bool {
     return true;
 }
 
-pub fn count(springs: &[char], groups: &[String]) -> usize {
-    let mut c = 0;
-    for i in 0..springs.len() {
-        if start_with(&springs[i..], &groups[0]) {
-            let remain = &springs[i+groups[0].len()..];
-            if remain.len() == 0 {
-                println!("remain len");
-                if groups.len() == 1 {
-                    c += 1;
-                }
-            }
-            else if remain[0] == '#' {
-                println!("remain is #");
-            }
-            else if groups.len() == 1 {
-                println!("groups.len == 1 (attention, il manque un cas)");
-                if remain.iter().all(|c| *c != '#') {
-                    c += 1;
-                }
-            }
-            else {
-                let remain = &springs[i+groups[0].len()+1..];
-                if remain.len() == 0 {
-                    if groups.len() == 1 {
-                        c += 1;
+pub fn count(springs: &[char], groups: &[String], cache: &mut HashMap<String, usize>) -> usize {
+    let mut key = String::from_iter(springs);
+    key.push_str(&groups.join(","));
+    let c = match cache.get(&key) {
+        Some(c) => *c,
+        None => {
+            let mut c = 0;
+            for i in 0..springs.len() {
+                if start_with(&springs[i..], &groups[0]) {
+                    let remain = &springs[i+groups[0].len()..];
+                    if remain.len() == 0 {
+                        if groups.len() == 1 {
+                            c += 1;
+                        }
+                    }
+                    else if remain[0] == '#' {
+                    }
+                    else if groups.len() == 1 {
+                        if remain.iter().all(|c| *c != '#') {
+                            c += 1;
+                        }
+                    }
+                    else {
+                        let remain = &springs[i+groups[0].len()+1..];
+                        if remain.len() == 0 {
+                            if groups.len() == 1 {
+                                c += 1;
+                            }
+                        }
+                        else {
+                            c += count(remain, &groups[1..], cache);
+                        }
                     }
                 }
-                else {
-                    c += count(remain, &groups[1..]);
+                if springs[i] == '#' {
+                    break;
                 }
             }
+            cache.insert(key.clone(), c);
+            c
         }
-        if springs[i] == '#' {
-            break;
-        }
-    }
-    println!("{springs:?} {groups:?} {c}");
+    };
+    println!("{key} {c}");
     c
 }
+                    /*
+                    let mut key = String::from_iter(remain);
+                    key.push_str(&groups[1..].join(","));
+                    c += match cache.get(&key) {
+                        Some(c) => *c,
+                        None => {
+                            let sub_c = count(remain, &groups[1..], cache);
+                            cache.insert(key, sub_c);
+                            let mut keyp = String::from_iter(remain);
+                            keyp.push_str(&groups[1..].join(","));
+                            println!("{keyp:?} {sub_c}");
+                            sub_c
+                        }
+                    };
+                    */
 
 pub fn read_groups(string: &str) -> Vec<String> {
     let controls: Vec<usize> = string.split(",").map(|n| n.parse().unwrap()).collect();
@@ -94,12 +118,24 @@ pub fn count_combination(line: &str) -> usize {
     let data: Vec<&str> = line.split_whitespace().collect();
     let springs: Vec<char> = data[0].chars().collect();
     let groups: Vec<String> = read_groups(data[1]);
-    println!("{:?}", groups);
-    count(&springs, &groups)
+    let mut cache: HashMap<String, usize> = HashMap::new();
+    count(&springs, &groups, &mut cache)
 }
 
 pub fn solve(input: &str) -> usize {
     input.lines().map(|l| count_combination(l)).sum()
+}
+
+pub fn solve2(input: &str) -> usize {
+    input.lines().enumerate().map(|(i, l)| {
+        let c = count_combination(&expand(l));
+        println!("{i} {l} {c}");
+        c
+    }).sum()
+}
+
+pub fn expand(input: &str) -> String {
+    RE.replace(input, "$1?$1?$1?$1?$1 $2,$2,$2,$2,$2").into_owned()
 }
 
 #[cfg(test)]
@@ -120,29 +156,32 @@ mod tests {
 
     #[test]
     fn it_count_with_question_marks() {
-        assert_eq!(count(&("??##".chars().collect::<Vec<char>>()), &read_groups("1,2")), 1);
-        assert_eq!(count(&("???##".chars().collect::<Vec<char>>()), &read_groups("1,2")), 2);
-        assert_eq!(count(&("?##".chars().collect::<Vec<char>>()), &read_groups("3")), 1);
-        assert_eq!(count(&("?#.".chars().collect::<Vec<char>>()), &read_groups("3")), 0);
-        assert_eq!(count(&("??##".chars().collect::<Vec<char>>()), &read_groups("3")), 1);
+        let mut cache: HashMap<String, usize> = HashMap::new();
+        assert_eq!(count(&("??##".chars().collect::<Vec<char>>()), &read_groups("1,2"), &mut cache), 1);
+        assert_eq!(count(&("???##".chars().collect::<Vec<char>>()), &read_groups("1,2"), &mut cache), 2);
+        assert_eq!(count(&("?##".chars().collect::<Vec<char>>()), &read_groups("3"), &mut cache), 1);
+        assert_eq!(count(&("?#.".chars().collect::<Vec<char>>()), &read_groups("3"), &mut cache), 0);
+        assert_eq!(count(&("??##".chars().collect::<Vec<char>>()), &read_groups("3"), &mut cache), 1);
     }
 
     #[test]
     fn it_count() {
-        assert_eq!(count(&("###".chars().collect::<Vec<char>>()), &read_groups("3")), 1);
-        assert_eq!(count(&("###..".chars().collect::<Vec<char>>()), &read_groups("3")), 1);
-        assert_eq!(count(&(".###".chars().collect::<Vec<char>>()), &read_groups("3")), 1);
-        assert_eq!(count(&("#.###".chars().collect::<Vec<char>>()), &read_groups("1,3")), 1);
-        assert_eq!(count(&("#.#.###".chars().collect::<Vec<char>>()), &read_groups("1,1,3")), 1);
-        assert_eq!(count(&("..###".chars().collect::<Vec<char>>()), &read_groups("1,3")), 0);
-        assert_eq!(count(&("#..##".chars().collect::<Vec<char>>()), &read_groups("1,3")), 0);
-        assert_eq!(count(&("#..###".chars().collect::<Vec<char>>()), &read_groups("1,3")), 1);
-        assert_eq!(count(&("##.###".chars().collect::<Vec<char>>()), &read_groups("1,3")), 0);
-        assert_eq!(count(&("#.#..##".chars().collect::<Vec<char>>()), &read_groups("1,1,3")), 0);
+        let mut cache: HashMap<String, usize> = HashMap::new();
+        assert_eq!(count(&("###".chars().collect::<Vec<char>>()), &read_groups("3"), &mut cache), 1);
+        assert_eq!(count(&("###..".chars().collect::<Vec<char>>()), &read_groups("3"), &mut cache), 1);
+        assert_eq!(count(&(".###".chars().collect::<Vec<char>>()), &read_groups("3"), &mut cache), 1);
+        assert_eq!(count(&("#.###".chars().collect::<Vec<char>>()), &read_groups("1,3"), &mut cache), 1);
+        assert_eq!(count(&("#.#.###".chars().collect::<Vec<char>>()), &read_groups("1,1,3"), &mut cache), 1);
+        assert_eq!(count(&("..###".chars().collect::<Vec<char>>()), &read_groups("1,3"), &mut cache), 0);
+        assert_eq!(count(&("#..##".chars().collect::<Vec<char>>()), &read_groups("1,3"), &mut cache), 0);
+        assert_eq!(count(&("#..###".chars().collect::<Vec<char>>()), &read_groups("1,3"), &mut cache), 1);
+        assert_eq!(count(&("##.###".chars().collect::<Vec<char>>()), &read_groups("1,3"), &mut cache), 0);
+        assert_eq!(count(&("#.#..##".chars().collect::<Vec<char>>()), &read_groups("1,1,3"), &mut cache), 0);
     }
 
     #[test]
     fn it_count_combinations() {
+        assert_eq!(count_combination(&expand("????.######..#####. 1,6,5")), 2500);
         assert_eq!(count_combination("????.#...#...# 4,1,1"), 0);
         assert_eq!(count_combination("?###???????? 3,2,1"), 10);
         assert_eq!(count_combination("???.### 1,1,3"), 1);
@@ -153,8 +192,15 @@ mod tests {
     }
 
     #[test]
+    fn it_expand() {
+        assert_eq!(expand(".# 1"), ".#?.#?.#?.#?.# 1,1,1,1,1");
+    }
+
+    #[test]
     fn it_works() {
         assert_eq!(solve(inputs::EXAMPLE), 21);
         assert_eq!(solve(inputs::INPUT), 7716);
+        assert_eq!(solve2(inputs::EXAMPLE), 525152);
+        assert_eq!(solve2(inputs::INPUT), 18716325559999);
     }
 }
