0855e2e34225e19635b5376841acb93fe4efc65d
Étienne Charignon <etienne.charignon@gmail.com>
2023-12-14 08:23:42 +0100

    Jour 13 partie 2
    Une petite frayeur avec un premier essaie raté puis je me suis souvenu
    qu'il ne falait pas regarder les lignes impaires
    (ça n'avait pas d'importance pour la partie 1 mais cette fois comme on
    compte le nombre de diff, c'est important)

--- 2023/j13/src/lib.rs
+++ 2023/j13/src/lib.rs
@@ -19,53 +19,112 @@ pub fn column(pattern: &Vec<Vec<char>>, i: usize) -> Vec<char> {
     pattern.iter().map(|l| l[i]).collect()
 }
 
+pub fn count_diff(p1: &Vec<char>, p2: &Vec<char>) -> usize {
+    let mut count = 0;
+    for i in 0..p1.len() {
+        if p1[i] != p2[i] {
+            count += 1;
+        } 
+    }
+    count
+}
+
 pub fn find_symetrie(pattern: &Vec<Vec<char>>) -> usize {
     println!("pattern {}", String::from_iter(&pattern[0]));
-    let mut sym = 0;
     let last_line = pattern.len() - 1;
     for r in 0..last_line {
-        if pattern[r] == pattern[last_line] {
-            let middle = (pattern.len() - r)/2;
-            println!("line {r} {last_line} {} {middle} {} {}", last_line + 1 - r, String::from_iter(&pattern[r]), String::from_iter(&pattern[last_line]));
-            if (1..middle).all(|i| pattern[r + i] == pattern[last_line - i]) {
-                println!("{}", middle + r);
+
+        if (pattern.len() - r) % 2 != 0 {
+            continue;
+        }
+        let mut diff = count_diff(&pattern[r], &pattern[last_line]);
+        let middle = (pattern.len() - r)/2;
+        if diff == 0 {
+            if (1..middle).map(|i| count_diff(&pattern[r + i], &pattern[last_line - i])).sum::<usize>() == 1 {
+                println!("pattern0 {}, {}", String::from_iter(&pattern[0]), middle + r);
                 return 100 * (middle + r);
             }
         }
-        if pattern[0] == pattern[last_line - r] {
-            let middle = (pattern.len() - r)/2;
-            println!("line- {} {last_line} {} {middle} {} {}",  last_line - r, last_line + 1 - r, String::from_iter(&pattern[r]), String::from_iter(&pattern[last_line]));
+        else if diff == 1 {
+            if (1..middle).all(|i| pattern[r + i] == pattern[last_line - i]) {
+                println!("pattern1 {}, {}", String::from_iter(&pattern[0]), middle + r);
+                return 100 * (middle + r);
+            }
+        }
+
+        diff = count_diff(&pattern[0], &pattern[last_line - r]);
+        if diff == 0 {
+            if (1..middle).map(|i| count_diff(&pattern[i], &pattern[last_line - r - i])).sum::<usize>() == 1 {
+                println!("pattern0- {}, {}", String::from_iter(&pattern[0]), middle);
+                return 100 * (middle);
+            }
+        }
+        else if diff == 1 {
             if (1..middle).all(|i| pattern[i] == pattern[last_line - r - i]) {
-                println!("{}", middle);
+                println!("pattern1- {}, {}", String::from_iter(&pattern[0]), middle);
                 return 100 * (middle);
             }
         }
     }
+
     let last_column = pattern[0].len() - 1;
     for c in 0..last_column {
-        // println!("{}, {} {}", c, String::from_iter(column(pattern, c)), String::from_iter(column(pattern, last_column)));
-        if column(pattern, c) == column(pattern, last_column) {
-            let middle = (last_column + 1 - c)/2;
-            println!("column {c} {last_column} {} {middle} {} {}", last_column + 1 - c, String::from_iter(&column(pattern, c)), String::from_iter(&column(pattern, last_column)));
-            if (1..middle).all(|i| column(pattern, c + i) == column(pattern, last_column - i)) {
-                println!("{}", middle + c);
+        if (last_column + 1 - c) % 2 != 0 {
+            continue;
+        }
+        let middle = (last_column + 1 - c)/2;
+        let mut diff = count_diff(&column(pattern, c), &column(pattern, last_column));
+        if diff == 0 {
+            if (1..middle).map(|i| count_diff(&column(pattern, c + i), &column(pattern, last_column - i))).sum::<usize>() == 1 {
+                println!("patternc0 {}, {}", String::from_iter(&pattern[0]), middle + c);
                 return middle + c;
             }
         }
-        if column(pattern, 0) == column(pattern, last_column - c) {
-            let middle = (last_column + 1 - c)/2;
-            println!("column- {} {last_column} {} {middle} {} {}", last_column - c, last_column + 1 - c, String::from_iter(&column(pattern, c)), String::from_iter(&column(pattern, last_column)));
+        else if diff == 1 {
+            if (1..middle).all(|i| column(pattern, c + i) == column(pattern, last_column - i)) {
+                println!("patternc1 {}, {}", String::from_iter(&pattern[0]), middle + c);
+                return middle + c;
+            }
+        }
+
+        diff = count_diff(&column(pattern, 0), &column(pattern, last_column - c));
+        if diff == 0 {
+            if (1..middle).map(|i| count_diff(&column(pattern, i), &column(pattern, last_column - c - i))).sum::<usize>() == 1 {
+                println!("patternc0- {}, {}", String::from_iter(&pattern[0]), middle);
+                return middle;
+            }
+        }
+        else if diff == 1 {
             if (1..middle).all(|i| column(pattern, i) == column(pattern, last_column - c - i)) {
-                println!("{}", middle);
+                println!("patternc1- {}, {}", String::from_iter(&pattern[0]), middle);
                 return middle;
             }
         }
     }
-    panic!("aucune symétrie {}", String::from_iter(&pattern[0]));
+    panic!("aucune tache trouvée");
 }
 
+/*
+pub fn search_smudge(p: &mut Vec<Vec<char>>) -> usize {
+    for r in 0..p.len() {
+        for c in 0..p[0].len() {
+            if let Some(elem) = p[r].get_mut(c) {
+                *elem = if *elem == '.' { '#' } else { '.' };
+            }
+            match find_symetrie(p) {
+                Some(n) => return n,
+                None => {
+                    if let Some(elem) = p[r].get_mut(c) {
+                        *elem = if *elem == '.' { '#' } else { '.' };
+                    }
+                }
+            }
+        }
+    }
+    panic!("Accune tache trouvée");
+}*/
+
 pub fn solve(patterns: &Vec<Vec<Vec<char>>>) -> usize {
-    println!("{:?}", patterns.iter().map(|p| find_symetrie(p)).collect::<Vec<_>>());
     patterns.iter().map(|p| find_symetrie(p)).sum()
 }
 
@@ -74,11 +133,15 @@ mod tests {
     use super::*;
 
     #[test]
+    fn it_count_diff() {
+        assert_eq!(count_diff(&("#.".chars().collect()), &("#.".chars().collect())), 0);
+        assert_eq!(count_diff(&("#.".chars().collect()), &("##".chars().collect())), 1);
+        assert_eq!(count_diff(&("..".chars().collect()), &("##".chars().collect())), 2);
+    }
+
+    #[test]
     fn it_works() {
-        assert_eq!(solve(&read(inputs::EXAMPLE)), 405);
-        assert_eq!(solve(&read(inputs::INPUT)), 35210); // 38575 to hight and 26764 is too low 28609 is wrong too, 37585 non.
+        assert_eq!(solve(&read(inputs::INPUT)), 31974); // too high
+        assert_eq!(solve(&read(inputs::EXAMPLE)), 400);
     }
 }
-// 29327 non
-// 31723 non
-// 32783 non
