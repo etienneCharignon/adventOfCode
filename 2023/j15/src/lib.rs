d5bcd556383d89364640079497b0073faaba1b8b
Étienne Charignon <etienne.charignon@gmail.com>
2023-12-15 10:26:18 +0100

    Jours 15 partie 2
    J'ai crue que j'allais souffir avec la mise à jour d'un élément d'une
    multimap, mais en utilisant un struct, pas de souci en fait.
    Je ne comprends même pas bien pourquoi je n'ai pas besoin de la déclarer
    mutable.

--- 2023/j15/src/lib.rs
+++ 2023/j15/src/lib.rs
@@ -1,5 +1,14 @@
 mod inputs;
 
+#[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
+pub struct Lense<'a>
+{
+    pub label: &'a str,
+    pub focal: usize
+}
+
+use multimap::MultiMap;
+
 pub fn hash(string: &str) -> usize {
     string.chars().fold(0, |hash, c| {
         let mut new_hash = hash + c as usize;
@@ -9,8 +18,50 @@ pub fn hash(string: &str) -> usize {
     })
 }
 
-pub fn solve(input: &str) -> usize {
+pub fn solve_p1(input: &str) -> usize {
     input.split(",").map(|c| hash(c)).sum()
+}
+
+pub fn focusing_power(input: &str) -> usize {
+    let mut boxes: MultiMap<usize, Lense> = MultiMap::new();
+    for step in input.split(",") {
+        let splitequal: Vec<&str> = step.split('=').collect();
+        if splitequal.len() == 2 {
+            println!("{splitequal:?}");
+            let box_number = hash(splitequal[0]);
+            let mut found = false;
+            for (n, list) in boxes.iter_all_mut() {
+                if box_number == *n {
+                    for lense in list {
+                        if lense.label == splitequal[0] {
+                            lense.focal = splitequal[1].parse::<usize>().unwrap();
+                            found = true;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            if ! found {
+                boxes.insert(box_number, Lense { label: splitequal[0], focal: splitequal[1].parse::<usize>().unwrap() });
+            }
+        }
+        else {
+            let label = step.split('-').collect::<Vec<_>>()[0];
+            println!("{label} => -");
+            let box_number = hash(label);
+            for (n, list) in boxes.iter_all_mut() {
+                if box_number == *n {
+                    list.retain(|l| label != l.label);
+                    break;
+                }
+            }
+        }
+        //println!("{boxes:?}");
+    }
+    boxes.iter_all().map(|(n, lenses)| {
+        lenses.iter().enumerate().map(|(i, lense)| (n + 1) * (i + 1) * lense.focal).sum::<usize>()
+    }).sum()
 }
 
 #[cfg(test)]
@@ -23,8 +74,14 @@ mod tests {
     }
 
     #[test]
-    fn it_solve_example() {
-        assert_eq!(solve(inputs::EXAMPLE), 1320);
-        assert_eq!(solve(inputs::INPUT), 519041);
+    fn it_solve_p1() {
+        assert_eq!(solve_p1(inputs::EXAMPLE), 1320);
+        assert_eq!(solve_p1(inputs::INPUT), 519041);
+    }
+
+    #[test]
+    fn it_solve_p2() {
+        assert_eq!(focusing_power(inputs::EXAMPLE), 145);
+        assert_eq!(focusing_power(inputs::INPUT), 260530);
     }
 }
