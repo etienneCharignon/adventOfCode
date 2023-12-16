3dce8bc4757214dce5df6b36567723f4cf640f3d
Étienne Charignon <etienne.charignon@gmail.com>
2023-12-16 13:21:43 +0100

    jour 16 partie 2
    Pas de finesse, j'ai juste calculé toutes les possiblité

--- 2023/j16/src/lib.rs
+++ 2023/j16/src/lib.rs
@@ -10,14 +10,13 @@ pub fn move_head(head: ((i32, i32),(i32, i32))) -> (i32, i32) {
     (head.0.0 + head.1.0, head.0.1 + head.1.1)
 }
 
-pub fn count_energised(start: ((i32, i32),(i32, i32)), input: Vec<Vec<char>>) -> usize {
+pub fn count_energised(start: ((i32, i32),(i32, i32)), input: &Vec<Vec<char>>) -> usize {
     let mut heads: Vec<((i32, i32),(i32, i32))> = vec![];
     let mut energised = MultiMap::<(i32, i32), (i32, i32)>::new();
     heads.push(start);
     while heads.len() > 0 {
         let head = heads.pop().unwrap();
         let new_pos = move_head(head);
-        println!("{new_pos:?}");
         if new_pos.0 < 0 || new_pos.1 < 0 || new_pos.0 == input[0].len() as i32 || new_pos.1 == input.len() as i32 {
             continue;
         }
@@ -35,7 +34,6 @@ pub fn count_energised(start: ((i32, i32),(i32, i32)), input: Vec<Vec<char>>) ->
             heads.push((new_pos, head.1));
         }
         else if input[y][x] == '\\' {
-            println!("\\");
             let new_direction = match head.1 {
                 (1, 0) => (0, 1),
                 (-1, 0) => (0, -1),
@@ -77,13 +75,30 @@ pub fn count_energised(start: ((i32, i32),(i32, i32)), input: Vec<Vec<char>>) ->
     energised.len()
 }
 
+pub fn count_max_energised(input: &Vec<Vec<char>>) -> usize {
+    let mut energized = Vec::<usize>::new();
+    let width = input[0].len() as i32;
+    let height = input.len() as i32;
+    for x in 0i32..width {
+        energized.push(count_energised(((x, -1),(0, 1)), input));
+        energized.push(count_energised(((x, height),(0, -1)), input));
+    }
+    *energized.iter().max().unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_solve() {
-        assert_eq!(count_energised(((-1, 0), (1, 0)), read(inputs::EXAMPLE)), 46);
-        assert_eq!(count_energised(((-1, 0), (1, 0)), read(inputs::INPUT)), 7608);
+    fn it_solve_p1() {
+        assert_eq!(count_energised(((-1, 0), (1, 0)), &read(inputs::EXAMPLE)), 46);
+        assert_eq!(count_energised(((-1, 0), (1, 0)), &read(inputs::INPUT)), 7608);
+    }
+
+    #[test]
+    fn it_solve_p2() {
+        assert_eq!(count_max_energised(&read(inputs::EXAMPLE)), 51);
+        assert_eq!(count_max_energised(&read(inputs::INPUT)), 8221);
     }
 }
