bccb12c65d49840f1891f03b6924ab5b68df3353
Étienne Charignon <etienne.charignon@gmail.com>
2023-12-27 08:37:11 +0100

    jour 22 partie 2
    La découverte tardive qu'une brique peut faire tomber toutes les briques
    de soutient d'une brick plus élevée et donc la faire tomber aussi.
    Résolution finale de l'exercice pedant un dessins annimé de 20 minutes
    après avoir réfléchi sous la douche \o/

--- 2023/j22/src/lib.rs
+++ 2023/j22/src/lib.rs
@@ -60,18 +60,18 @@ pub fn find_max_z(obrick: Option<Brick>, max_z: i64) -> i64 {
     }
 }
 
-pub fn add_supporters(supporters: &mut MultiMap<Brick, Brick>, brick: Brick, supporter: Brick) {
-    match supporters.get_vec(&brick) {
-        None => supporters.insert(brick, supporter),
+pub fn push_unique(map: &mut MultiMap<Brick, Brick>, key: Brick, value: Brick) {
+    match map.get_vec(&key) {
+        None => map.insert(key, value),
         Some(existing_supporters) => {
-            if ! existing_supporters.contains(&supporter) {
-                supporters.insert(brick, supporter);
+            if ! existing_supporters.contains(&value) {
+                map.insert(key, value);
             }
         }
     }
 }
 
-pub fn move_brick(brick: Brick, surface: &mut Vec<Vec<Option<Brick>>>, supporters: &mut MultiMap<Brick, Brick>) -> Brick {
+pub fn move_brick(brick: Brick, surface: &mut Vec<Vec<Option<Brick>>>, supporters: &mut MultiMap<Brick, Brick>, supported: &mut MultiMap<Brick, Brick>) -> Brick {
     let minx = brick.0.0 as usize;
     let maxx = brick.1.0 as usize;
     let miny = brick.0.1 as usize;
@@ -87,7 +87,8 @@ pub fn move_brick(brick: Brick, surface: &mut Vec<Vec<Option<Brick>>>, supporter
                 None => {},
                 Some(brick) => {
                     if brick.1.2 == max_surface_z {
-                        add_supporters(supporters, new_brick, brick);
+                        push_unique(supporters, new_brick, brick);
+                        push_unique(supported, brick, new_brick);
                     }
                 }
             }
@@ -108,7 +109,8 @@ pub fn move_brick(brick: Brick, surface: &mut Vec<Vec<Option<Brick>>>, supporter
                 None => {},
                 Some(brick) => {
                     if brick.1.2 == max_surface_z {
-                        add_supporters(supporters, new_brick, brick);
+                        push_unique(supporters, new_brick, brick);
+                        push_unique(supported, brick, new_brick);
                     }
                 }
             }
@@ -125,7 +127,8 @@ pub fn move_brick(brick: Brick, surface: &mut Vec<Vec<Option<Brick>>>, supporter
             None => {},
             Some(brick) => {
                 if brick.1.2 == max_surface_z {
-                    add_supporters(supporters, new_brick, brick);
+                    push_unique(supporters, new_brick, brick);
+                    push_unique(supported, brick, new_brick);
                 }
             }
         }
@@ -150,8 +153,8 @@ pub fn letter(position: usize) -> char {
     ('A' as u8 + TryInto::<u8>::try_into(position).unwrap()) as char
 }
 
-pub fn print_stack(bricks: &HashSet<Brick>) {
-    let mut stack = Vec::from_iter(bricks.iter());
+pub fn print_stack(bricks: &Vec<Brick>) {
+    let mut stack = bricks.clone();
     stack.sort_by(|b1, b2| b1.min_x().cmp(&b2.min_x()));
     stack.sort_by(|b1, b2| b1.min_y().cmp(&b2.min_y()));
     stack.sort_by(|b1, b2| b1.min_z().cmp(&b2.min_z()));
@@ -194,8 +197,7 @@ pub fn print_stack(bricks: &HashSet<Brick>) {
     println!("");
 }
 
-
-pub fn count_candidate(stack: Vec<Brick>) -> usize {
+pub fn move_all_bricks(stack: Vec<Brick>) -> (Vec<Brick>, MultiMap<Brick, Brick>, MultiMap<Brick, Brick>, HashSet<Brick>) {
     let mut sorted_z = stack;
     sorted_z.sort_by(|b1, b2| b1.min_z().cmp(&b2.min_z()));
     // println!("{sorted_z:?}");
@@ -206,31 +208,57 @@ pub fn count_candidate(stack: Vec<Brick>) -> usize {
     let mut surface = new_surface(width, depth);
 
     let mut supporters = MultiMap::<Brick, Brick>::new();
-    let mut desintegrable = HashSet::<Brick>::new();
-    // let mut dropping = HashSet::<Brick>::new();
+    let mut supported = MultiMap::<Brick, Brick>::new();
+    let mut desintegrables = HashSet::<Brick>::new();
+    let mut stack = Vec::<Brick>::new();
     for brick in sorted_z {
-        // dropping.insert(brick);
-        // if dropping.len() < 26 {
-        //     println!("Dropping");
-        //     print_stack(&dropping);
-        // }
-        let moved_brick = move_brick(brick, &mut surface, &mut supporters);
-        desintegrable.insert(moved_brick);
-        // if desintegrable.len() < 26 {
-        //     println!("SUPPORTERS");
-        //     for (brick, bricks) in supporters.iter_all() {
-        //         println!("{brick:?} : {} {bricks:?}", bricks.len());
-        //     }
-        //     println!("Dropped");
-        //     print_stack(&desintegrable);
-        // }
+        let moved_brick = move_brick(brick, &mut surface, &mut supporters, &mut supported);
+        desintegrables.insert(moved_brick);
+        stack.push(moved_brick);
     }
     for (_brick, bricks) in supporters.iter_all() {
         if bricks.len() == 1 {
-            desintegrable.remove(&bricks[0]);
+            desintegrables.remove(&bricks[0]);
         }
     }
-    desintegrable.len()
+    (stack, supporters, supported, desintegrables)
+
+}
+
+pub fn count_candidate(stack: Vec<Brick>) -> usize {
+    let (_stack, _supporters, _supported, desintegrables) = move_all_bricks(stack);
+    desintegrables.len()
+}
+
+pub fn find_fallings(brick: Brick, supporteds: &MultiMap<Brick, Brick>, supporters: &MultiMap<Brick, Brick>, fallings: &mut HashSet<Brick>) {
+    let parents = supporteds.get_vec(&brick);
+    if parents == None {
+        return;
+    }
+    let mut new_falling = HashSet::<Brick>::new();
+    for parent in parents.unwrap() {
+        let children = supporters.get_vec(&parent).unwrap();
+        if children.iter().all(|child| *child == brick || fallings.contains(child)) {
+            fallings.insert(*parent);
+            new_falling.insert(*parent);
+        }
+    }
+    for parent in new_falling {
+        find_fallings(parent, supporteds, supporters, fallings)
+    }
+}
+
+pub fn count_fallings(stack: Vec<Brick>) -> usize {
+    let (stack, supporters, supported, desintegrables) = move_all_bricks(stack);
+    // print_stack(&stack);
+    // println!("{supported:?}");
+    let mut count = 0;
+    for brick in stack {
+        let mut fallings = HashSet::<Brick>::new();
+        find_fallings(brick, &supported, &supporters, &mut fallings);
+        count += fallings.len();
+    }
+    count
 }
 
 #[cfg(test)]
@@ -240,11 +268,13 @@ mod tests {
     #[test]
     fn it_move_brick_on_z() {
         let mut supporters = MultiMap::<Brick, Brick>::new();
+        let mut supported = MultiMap::<Brick, Brick>::new();
         let mut surface = new_surface(3, 3);
-        move_brick(Brick::new(vec!["0,0,1", "0,0,3"]), &mut surface, &mut supporters);
+        move_brick(Brick::new(vec!["0,0,1", "0,0,3"]), &mut surface, &mut supporters, &mut supported);
         assert_eq!(supporters.len(), 0);
+        assert_eq!(supported.len(), 0);
         assert_eq!(surface[0][0].unwrap(), Brick::new(vec!["0,0,1", "0,0,3"]));
-        move_brick(Brick::new(vec!["0,0,6", "0,0,7"]), &mut surface, &mut supporters);
+        move_brick(Brick::new(vec!["0,0,6", "0,0,7"]), &mut surface, &mut supporters, &mut supported);
         assert_eq!(surface[0][0].unwrap(), Brick::new(vec!["0,0,4", "0,0,5"]));
         assert_eq!(supporters.len(), 1);
         assert_eq!(supporters.get(&Brick::new(vec!["0,0,4", "0,0,5"])), Some(&Brick::new(vec!["0,0,1", "0,0,3"])));
@@ -253,12 +283,13 @@ mod tests {
     #[test]
     fn it_move_brick_on_x() {
         let mut supporters = MultiMap::<Brick, Brick>::new();
+        let mut supported = MultiMap::<Brick, Brick>::new();
         let mut surface = new_surface(3, 3);
-        move_brick(Brick::new(vec!["0,0,1", "1,0,1"]), &mut surface, &mut supporters);
+        move_brick(Brick::new(vec!["0,0,1", "1,0,1"]), &mut surface, &mut supporters, &mut supported);
         assert_eq!(supporters.len(), 0);
         assert_eq!(surface[0][0].unwrap(), Brick::new(vec!["0,0,1", "1,0,1"]));
         assert_eq!(surface[1][0].unwrap(), Brick::new(vec!["0,0,1", "1,0,1"]));
-        move_brick(Brick::new(vec!["1,0,3", "1,2,3"]), &mut surface, &mut supporters);
+        move_brick(Brick::new(vec!["1,0,3", "1,2,3"]), &mut surface, &mut supporters, &mut supported);
         assert_eq!(surface[1][0].unwrap(), Brick::new(vec!["1,0,2", "1,2,2"]));
         assert_eq!(surface[1][1].unwrap(), Brick::new(vec!["1,0,2", "1,2,2"]));
         assert_eq!(surface[1][2].unwrap(), Brick::new(vec!["1,0,2", "1,2,2"]));
@@ -269,13 +300,14 @@ mod tests {
     #[test]
     fn it_move_brick_on_y() {
         let mut supporters = MultiMap::<Brick, Brick>::new();
+        let mut supported = MultiMap::<Brick, Brick>::new();
         let mut surface = new_surface(3, 3);
-        move_brick(Brick::new(vec!["1,0,1", "1,2,1"]), &mut surface, &mut supporters);
+        move_brick(Brick::new(vec!["1,0,1", "1,2,1"]), &mut surface, &mut supporters, &mut supported);
         assert_eq!(supporters.len(), 0);
         assert_eq!(surface[1][0].unwrap(), Brick::new(vec!["1,0,1", "1,2,1"]));
         assert_eq!(surface[1][1].unwrap(), Brick::new(vec!["1,0,1", "1,2,1"]));
         assert_eq!(surface[1][2].unwrap(), Brick::new(vec!["1,0,1", "1,2,1"]));
-        move_brick(Brick::new(vec!["1,0,3", "1,2,3"]), &mut surface, &mut supporters);
+        move_brick(Brick::new(vec!["1,0,3", "1,2,3"]), &mut surface, &mut supporters, &mut supported);
         assert_eq!(surface[1][0].unwrap(), Brick::new(vec!["1,0,2", "1,2,2"]));
         assert_eq!(surface[1][1].unwrap(), Brick::new(vec!["1,0,2", "1,2,2"]));
         assert_eq!(surface[1][2].unwrap(), Brick::new(vec!["1,0,2", "1,2,2"]));
@@ -287,5 +319,11 @@ mod tests {
     fn it_count_candidate() {
         assert_eq!(count_candidate(read(inputs::EXAMPLE)), 5);
         assert_eq!(count_candidate(read(inputs::INPUT)), 405); // 418 to high, 283 to low
+    }
+
+    #[test]
+    fn it_count_fallings() {
+        assert_eq!(count_fallings(read(inputs::EXAMPLE)), 7);
+        assert_eq!(count_fallings(read(inputs::INPUT)), 61297); // 1291 to low
     }
 }
