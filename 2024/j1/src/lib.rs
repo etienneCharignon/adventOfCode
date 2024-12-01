c63910ef8c69bd1fe4f27dd27060b4599f60fd16
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-01 10:36:19 +0100

    J1 partie 2

--- 2024/j1/src/lib.rs
+++ 2024/j1/src/lib.rs
@@ -7,10 +7,10 @@ mod tests {
     #[test]
     fn it_works() {
         let lines = inputs::INPUT.split('\n');
-        let mut list1: Vec<i32> = vec![];
-        let mut list2: Vec<i32> = vec![];
+        let mut list1: Vec<usize> = vec![];
+        let mut list2: Vec<usize> = vec![];
         for line in lines {
-            let numbers: Vec<i32> = line
+            let numbers: Vec<usize> = line
                 .split_whitespace()
                 .map(|s| s.parse().expect("Failed to parse number"))
                 .collect();
@@ -20,14 +20,14 @@ mod tests {
         list1.sort();
         list2.sort();
 
-        let mut distance = 0;
-        for (index, n1) in list1.iter().enumerate() {
-            let d = (n1 - list2[index]).abs();
-            println!("{d}");
-            distance = distance + d;
+        let mut similarity = 0;
+        for n1 in list1.iter() {
+            let s = n1 * list2.iter().filter(|&n| *n == *n1).count();
+            println!("{s}");
+            similarity = similarity + s;
         }
 
 
-        assert_eq!(distance, 3574690);
+        assert_eq!(similarity, 22565391);
     }
 }
