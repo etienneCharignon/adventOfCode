5a12c006c6cbc499c8707c78f5b1d02570e56158
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-01 11:32:28 +0100

    Supprime des warning et réécrit en fonctionnel

--- 2024/j1/src/lib.rs
+++ 2024/j1/src/lib.rs
@@ -7,25 +7,22 @@ mod tests {
     #[test]
     fn it_works() {
         let lines = inputs::INPUT.split('\n');
-        let mut list1: Vec<usize> = vec![];
-        let mut list2: Vec<usize> = vec![];
-        for line in lines {
+        let (mut list1, mut list2): (Vec<usize>, Vec<usize>) = lines.into_iter().map(|line| {
             let numbers: Vec<usize> = line
                 .split_whitespace()
                 .map(|s| s.parse().expect("Failed to parse number"))
                 .collect();
-            list1.push(numbers[0]);
-            list2.push(numbers[1]);
-        }
+            (numbers[0], numbers[1])
+        }).unzip();
+
         list1.sort();
         list2.sort();
 
-        let mut similarity = 0;
-        for n1 in list1.iter() {
-            let s = n1 * list2.iter().filter(|&n| *n == *n1).count();
-            println!("{s}");
-            similarity = similarity + s;
-        }
+
+        let similarity: usize = list1.iter().map(|n1|
+            *n1 * list2.iter().filter(|&n| *n == *n1).count()
+            )
+            .sum();
 
 
         assert_eq!(similarity, 22565391);
