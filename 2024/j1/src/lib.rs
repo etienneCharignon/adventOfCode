2b873e373b1bd1c83b86da8abb6f380feb2a1549
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-01 11:57:11 +0100

    Sort est inutile pour la partie 2

--- 2024/j1/src/lib.rs
+++ 2024/j1/src/lib.rs
@@ -7,16 +7,13 @@ mod tests {
     #[test]
     fn it_works() {
         let lines = inputs::INPUT.split('\n');
-        let (mut list1, mut list2): (Vec<usize>, Vec<usize>) = lines.into_iter().map(|line| {
+        let (list1, list2): (Vec<usize>, Vec<usize>) = lines.into_iter().map(|line| {
             let numbers: Vec<usize> = line
                 .split_whitespace()
                 .map(|s| s.parse().expect("Failed to parse number"))
                 .collect();
             (numbers[0], numbers[1])
         }).unzip();
-
-        list1.sort();
-        list2.sort();
 
 
         let similarity: usize = list1.iter().map(|n1|
