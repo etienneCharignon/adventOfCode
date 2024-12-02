b0eb9ecf5f53db1f93da0016606ea4b55b1aa7b0
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-02 08:20:33 +0100

    j2 part 1

---
+++ 2024/j2/src/lib.rs
@@ -0,0 +1,41 @@
+mod inputs;
+
+fn safe(levels: Vec<i32>) -> bool {
+    let direction = (levels[0] - levels[1]) < 0;
+    for i in 1..levels.len() {
+        let increase = levels[i-1] - levels[i];
+        if (increase < 0) != direction { return false; }
+        if increase.abs() < 1 || increase.abs() > 3 { return false; }
+    }
+    true
+}
+
+fn count_safe(input: &str) -> i32 {
+    let lines = input.split('\n');
+    let mut count = 0;
+    for line in  lines {
+        let levels = line.split_whitespace().map(|s| s.parse().expect("Failed to parse number")).collect();
+        if safe(levels) { count += 1 }
+    }
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_say_safe() {
+        assert_eq!(safe(vec![1, 2, 3]), true);
+        assert_eq!(safe(vec![3, 2, 1]), true);
+        assert_eq!(safe(vec![1, 2, 1]), false);
+        assert_eq!(safe(vec![1, 1, 2]), false);
+        assert_eq!(safe(vec![3, 1, 1]), false);
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(count_safe(inputs::EXAMPLE), 2);
+        assert_eq!(count_safe(inputs::INPUT), 549);
+    }
+}
