475187e548c97b28e353f78aa85ac0ebc63acdf9
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-02 08:32:03 +0100

    j2 part 2

--- 2024/j2/src/lib.rs
+++ 2024/j2/src/lib.rs
@@ -1,6 +1,6 @@
 mod inputs;
 
-fn safe(levels: Vec<i32>) -> bool {
+pub fn safe(levels: Vec<i32>) -> bool {
     let direction = (levels[0] - levels[1]) < 0;
     for i in 1..levels.len() {
         let increase = levels[i-1] - levels[i];
@@ -10,12 +10,22 @@ fn safe(levels: Vec<i32>) -> bool {
     true
 }
 
-fn count_safe(input: &str) -> i32 {
+pub fn count_safe(input: &str) -> i32 {
     let lines = input.split('\n');
     let mut count = 0;
     for line in  lines {
-        let levels = line.split_whitespace().map(|s| s.parse().expect("Failed to parse number")).collect();
-        if safe(levels) { count += 1 }
+        let levels: Vec<i32> = line.split_whitespace().map(|s| s.parse().expect("Failed to parse number")).collect();
+        if safe(levels.clone()) { count += 1 }
+        else {
+            for i in 0..levels.len() {
+                let mut levels_but_one = levels.clone();
+                levels_but_one.remove(i);
+                if safe(levels_but_one) {
+                    count += 1;
+                    break;
+                }
+            }
+        }
     }
     count
 }
@@ -35,7 +45,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(count_safe(inputs::EXAMPLE), 2);
-        assert_eq!(count_safe(inputs::INPUT), 549);
+        assert_eq!(count_safe(inputs::EXAMPLE), 4);
+        assert_eq!(count_safe(inputs::INPUT), 589);
     }
 }
