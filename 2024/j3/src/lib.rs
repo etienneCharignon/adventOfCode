fa707a3dd9bbb7fcc1daa75e6b8b12da4baf1745
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-03 10:53:59 +0100

    j3 partie 3

--- 2024/j3/src/lib.rs
+++ 2024/j3/src/lib.rs
@@ -17,13 +17,40 @@ pub fn compute_uncorrupted(input: &str) -> i32 {
     sum
 }
 
+pub fn compute_uncorrupted_but_dont(input:&str) ->i32 {
+    let mut sum = 0;
+    match input.find("don't()") {
+        Some(donot_index) => {
+            let enabled = &input[..donot_index];
+            sum += compute_uncorrupted(enabled);
+            let following = &input[donot_index..];
+            match following.find("do()") {
+                Some(do_index) => {
+                    sum += compute_uncorrupted_but_dont(&following[do_index..]);
+                },
+                _ => {}
+            }
+        },
+        _ => {
+            sum += compute_uncorrupted(input);
+        }
+    }
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works_part1() {
         assert_eq!(compute_uncorrupted(inputs::EXAMPLE), 161);
         assert_eq!(compute_uncorrupted(inputs::INPUT), 185797128);
+    }
+
+    #[test]
+    fn it_works_part2() {
+        assert_eq!(compute_uncorrupted_but_dont(inputs::EXAMPLE), 48);
+        assert_eq!(compute_uncorrupted_but_dont(inputs::INPUT), 89798695);
     }
 }
