8c4f9007e84d5981a8763b5f9ce8d1fb163275cc
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-05 09:09:19 +0100

    j5 partie 2

--- 2024/j5/src/lib.rs
+++ 2024/j5/src/lib.rs
@@ -1,5 +1,6 @@
 mod inputs;
 use multimap::MultiMap;
+use std::cmp::Ordering;
 
 pub fn read_priorities(priorities: &str) -> MultiMap<i32, i32> {
     let mut map = MultiMap::new();
@@ -40,6 +41,48 @@ pub fn solve_p1(priorities: MultiMap<i32, i32>, updates: &str) -> i32 {
     sum
 }
 
+pub fn compare(priorities: &MultiMap<i32, i32>, p1: &i32, p2: &i32) -> Ordering {
+    match priorities.get_vec(p1) {
+        Some(after_pages) => {
+            if after_pages.contains(p2) {
+                Ordering::Less
+            }
+            else {
+                Ordering::Greater
+            }
+        },
+        _ => {
+            match priorities.get_vec(p2) {
+                Some(after_pages) => {
+                    if after_pages.contains(p1) {
+                        Ordering::Greater
+                    }
+                    else {
+                        Ordering::Less
+                    }
+                },
+                _ => {
+                    Ordering::Equal
+                }
+            }
+        }
+    }
+}
+
+pub fn solve_p2(priorities: MultiMap<i32, i32>, updates: &str) -> i32 {
+    let mut sum = 0;
+    for line in updates.split("\n") {
+        let mut pages: Vec<i32> = line.split(',')
+                .map(|s| s.parse().expect("Failed to parse number"))
+                .collect();
+        if !good_priorities(&priorities, &pages){
+            pages.sort_by(|a, b| compare(&priorities, a,b));
+            sum += pages[pages.len() / 2];
+        }
+    }
+    sum
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -51,8 +94,14 @@ mod tests {
     }
 
     #[test]
-    fn it_solve() {
+    fn it_solve_p1() {
         assert_eq!(solve_p1(read_priorities(inputs::EXAMPLE_PRIORITIES), inputs::EXAMPLE_UPDATES), 143);
-        assert_eq!(solve_p1(read_priorities(inputs::INPUT_PRIORITIES), inputs::INPUT_UPDATES), 143);
+        assert_eq!(solve_p1(read_priorities(inputs::INPUT_PRIORITIES), inputs::INPUT_UPDATES), 5064);
+    }
+
+    #[test]
+    fn it_solve_p2() {
+        assert_eq!(solve_p2(read_priorities(inputs::EXAMPLE_PRIORITIES), inputs::EXAMPLE_UPDATES), 123);
+        assert_eq!(solve_p2(read_priorities(inputs::INPUT_PRIORITIES), inputs::INPUT_UPDATES), 5152);
     }
 }
