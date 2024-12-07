c0d67ec05c3a4a258fec7e7a365aa716b852cfb6
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-07 13:27:26 +0100

    Quelques améliorations d'écriture

--- 2024/j7/src/lib.rs
+++ 2024/j7/src/lib.rs
@@ -1,12 +1,12 @@
 mod inputs;
 
-pub fn terminate_by(value: i64, last_digit: i64) -> bool {
-    let len = format!("{last_digit}").len() as u32;
+pub fn terminate_by(value: i64, last_digit: &i64) -> bool {
+    let len = last_digit.to_string().len() as u32;
     (value-last_digit) % (10_i64.pow(len)) == 0
 }
 
-pub fn remove_last(value: i64, last_digit: i64) -> i64 {
-    let len = format!("{last_digit}").len() as u32;
+pub fn remove_last(value: i64, last_digit: &i64) -> i64 {
+    let len = last_digit.to_string().len() as u32;
     (value-last_digit) / (10_i64.pow(len)) 
 }
 
@@ -16,8 +16,9 @@ pub fn check_equation(test_value: i64, terms: &[i64]) -> bool {
             format!("{test_value}") == format!("{}{}", terms[0], terms[1])
     }
     else {
-        let previous = &terms[..terms.len() -1];
-        let last_term = terms[terms.len()-1];
+        let Some((last_term, previous)) = terms.split_last() else {
+            return false;
+        };
         check_equation(test_value - last_term, previous) || 
             test_value % last_term == 0 && check_equation(test_value / last_term, previous) ||
             terminate_by(test_value, last_term) && check_equation(remove_last(test_value, last_term), previous)
@@ -45,16 +46,16 @@ mod tests {
 
     #[test]
     fn it_check_terminante_by() {
-            assert_eq!(terminate_by(486, 6), true);
-            assert_eq!(terminate_by(4866, 66), true);
-            assert_eq!(terminate_by(4866, 65), false);
-            assert_eq!(terminate_by(4856, 66), false);
+            assert_eq!(terminate_by(486, &6), true);
+            assert_eq!(terminate_by(4866, &66), true);
+            assert_eq!(terminate_by(4866, &65), false);
+            assert_eq!(terminate_by(4856, &66), false);
     }
 
     #[test]
     fn it_check_remove_last() {
-            assert_eq!(remove_last(486, 6), 48);
-            assert_eq!(remove_last(4866, 66), 48);
+            assert_eq!(remove_last(486, &6), 48);
+            assert_eq!(remove_last(4866, &66), 48);
     }
 
     #[test]
