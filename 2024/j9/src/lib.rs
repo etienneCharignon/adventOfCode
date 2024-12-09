0137ebbbc21b8a56b522f9c07f73d7722eeed041
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-09 18:57:26 +0100

    j9 partie 2

--- 2024/j9/src/lib.rs
+++ 2024/j9/src/lib.rs
@@ -1,7 +1,24 @@
 mod inputs;
 
 
-pub fn checksum(input: &str) -> usize {
+#[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
+pub struct File {
+    pub id: usize,
+    pub size: u64
+}
+
+#[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
+pub struct Free {
+    pub size: u64
+}
+
+#[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
+pub enum Segment {
+    File(File),
+    Free(Free)
+}
+
+pub fn checksum_d1(input: &str) -> usize {
     let mut checksum = 0;
     let diskmap: Vec<u64> = input.chars().map(|c| c.to_string().parse().expect("failed to parse number")).collect();
     let mut ongoing_i = 0;
@@ -52,13 +69,90 @@ pub fn checksum(input: &str) -> usize {
     checksum
 }
 
+pub fn find_index_of_first_free_space(diskmap: &Vec<Segment>, file: &File) -> usize {
+    for (index, segment) in diskmap.into_iter().enumerate() {
+        match segment {
+            Segment::File(s) => {
+                if s.id == file.id {
+                    return 0;
+                }
+            },
+            Segment::Free(s) => { 
+                if s.size >= file.size {
+                    return index;
+                }
+            }
+        }
+    }
+    0
+}
+
+pub fn checksum_d2(input: &str) -> usize {
+    let data: Vec<u64> = input.chars().map(|c| c.to_string().parse().expect("failed to parse number")).collect();
+    let mut diskmap: Vec<Segment> = vec![];
+    let mut files: Vec<File> = vec![];
+    for (index, f) in data.into_iter().enumerate() {
+        if index % 2 == 0 {
+            let file = File { id: index/2, size: f };
+            diskmap.push(Segment::File(file));
+            files.push(file);
+        }
+        else {
+            diskmap.push(Segment::Free(Free { size: f }));
+        }
+    }
+
+    for file in files.iter().rev() {
+        let i = find_index_of_first_free_space(&diskmap, &file);
+        println!("first free space : {i} : {:?}", file);
+        if i > 0 {
+            let Segment::Free(free) = diskmap.remove(i) else { panic!("i must be a free space") };
+            let from = diskmap.iter().position(|&segment| {
+                match segment {
+                    Segment::File(s) => { s.id == file.id },
+                    Segment::Free(_) => { false }
+                }
+            }).unwrap();
+            diskmap[from] = Segment::Free(Free { size: file.size });
+            diskmap.insert(i, Segment::File(*file));
+            let remaining_space = free.size - file.size;
+            if remaining_space > 0 {
+                diskmap.insert(i + 1, Segment::Free(Free { size: remaining_space }));
+            }
+        }
+    }
+    println!("===========");
+
+    let mut checksum = 0;
+    let mut block_position = 0;
+    for s in diskmap {
+        match s {
+            Segment::Free(s) => { block_position += s.size },
+            Segment::File(s) => { 
+                for _ in 0..s.size {
+                    checksum += s.id * block_position as usize;
+                    block_position += 1;
+                }
+            }
+        }
+        println!("segment : {:?}, checksum {}, block_position {block_position}", s, checksum);
+    }
+    checksum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(checksum(inputs::EXAMPLE), 1928);
-        assert_eq!(checksum(inputs::INPUT), 6334655979668);
+    fn it_checksum_d1() {
+        assert_eq!(checksum_d1(inputs::EXAMPLE), 1928);
+        assert_eq!(checksum_d1(inputs::INPUT), 6334655979668);
+    }
+
+    #[test]
+    fn it_checksum_j2() {
+        assert_eq!(checksum_d2(inputs::EXAMPLE), 2858);
+        assert_eq!(checksum_d2(inputs::INPUT), 6349492251099);
     }
 }
