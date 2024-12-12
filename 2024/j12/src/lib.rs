945ddd8a69ad50d1f24e30a17dcf28faf77e78b7
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-12 11:54:28 +0100

    j12 partie 2

--- 2024/j12/src/lib.rs
+++ 2024/j12/src/lib.rs
@@ -23,10 +23,9 @@ pub fn read(input: &str) -> (Vec<Vec<char>>, i32, i32) {
     (world, height as i32, width as i32)
 }
 
-pub fn explore_region(pos: Pos, map: &Vec<Vec<char>>, height: i32, width: i32, visited: &mut HashSet<Pos>) -> (usize, usize) {
+pub fn explore_region(pos: Pos, map: &Vec<Vec<char>>, height: i32, width: i32, visited: &mut HashSet<Pos>, fences: &mut Vec<(Pos, Pos)>) -> usize {
     let plant = map[pos.y as usize][pos.x as usize];
     let mut area = 0;
-    let mut fences = 0;
     while !visited.contains(&pos) {
         area += 1;
         visited.insert(pos);
@@ -34,20 +33,46 @@ pub fn explore_region(pos: Pos, map: &Vec<Vec<char>>, height: i32, width: i32, v
         for d in directions {
             let neighboug = add(pos, d);
             if neighboug.x < 0 || neighboug.y < 0 || neighboug.x >=width || neighboug.y >= height {
-                fences += 1;
+                fences.push((neighboug, d));
                 continue;
             }
             if plant == map[neighboug.y as usize][neighboug.x as usize] {
-                let (a, f)= explore_region(neighboug, map, height, width, visited);
+                let a = explore_region(neighboug, map, height, width, visited, fences);
                 area += a;
-                fences += f;
             }
             else {
-                fences +=1;
+                fences.push((neighboug, d));
             }
         }
     }
-    (area, fences)
+    area
+}
+
+pub fn visite_side(pos: (Pos, Pos), visited: &mut HashSet<(Pos, Pos)>, fences: &Vec<(Pos, Pos)>) {
+    let directions = [Pos{x:1, y:0},Pos{x:0, y:1}, Pos{x:-1, y:0}, Pos{x:0, y:-1}];
+    for d in directions {
+        let neighboug = add(pos.0, d);
+        let neighbour_fence = (neighboug, pos.1);
+        if fences.contains(&neighbour_fence) && !visited.contains(&neighbour_fence) {
+            visited.insert(neighbour_fence);
+            visite_side(neighbour_fence, visited, fences);
+        }
+    }
+}
+
+pub fn count_sides(fences: &Vec<(Pos, Pos)>) -> usize {
+    let mut visited = HashSet::<(Pos, Pos)>::new();
+    let mut sides = 0;
+    println!("{:?}", fences);
+    for f in fences {
+        if visited.contains(f) {
+            continue;
+        }
+        visited.insert(*f);
+        sides += 1;
+        visite_side(*f, &mut visited, fences);
+    }
+    sides
 }
 
 pub fn compute_price(world: (Vec<Vec<char>>, i32, i32)) -> usize {
@@ -58,8 +83,11 @@ pub fn compute_price(world: (Vec<Vec<char>>, i32, i32)) -> usize {
         for y in 0..height {
             let pos = Pos {x: x as i32, y: y as i32};
             if ! visited.contains(&pos) {
-                let (a, f) = explore_region(pos, &map, height, width, &mut visited);
-                price += a * f;
+                let mut fences = vec![];
+                let a = explore_region(pos, &map, height, width, &mut visited, &mut fences);
+                let sides = count_sides(&fences);
+                println!("{sides}");
+                price += a * sides;
             }
         }
     }
@@ -71,17 +99,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_exploire_region() {
+    fn it_explore_region() {
         let (map, height, width) = read(inputs::EXAMPLE_X);
         let mut visited = HashSet::<Pos>::new();
-        assert_eq!(explore_region(Pos{x:1, y:1}, &map, height, width, &mut visited), (1,4));
+        let mut fences = vec![];
+        assert_eq!(explore_region(Pos{x:1, y:1}, &map, height, width, &mut visited, &mut fences), 1);
         let mut visited = HashSet::<Pos>::new();
-        assert_eq!(explore_region(Pos{x:0, y:0}, &map, height, width, &mut visited), (21, 36));
+        let mut fences = vec![];
+        assert_eq!(explore_region(Pos{x:0, y:0}, &map, height, width, &mut visited, &mut fences), 21);
+        assert_eq!(fences.len(), 36)
     }
 
     #[test]
     fn it_works() {
-        assert_eq!(compute_price(read(inputs::EXAMPLE)), 1930);
-        assert_eq!(compute_price(read(inputs::INPUT)), 1363682);
+        assert_eq!(compute_price(read(inputs::EXAMPLE_E)), 236);
+        assert_eq!(compute_price(read(inputs::INPUT)), 787680);
     }
 }
