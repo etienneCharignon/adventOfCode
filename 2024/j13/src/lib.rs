6255587d474760f7db6e56c44724b20adf8b477f
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-13 09:46:21 +0100

    j13 partie 2

--- 2024/j13/src/lib.rs
+++ 2024/j13/src/lib.rs
@@ -10,8 +10,8 @@ lazy_static! {
 
 #[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
 pub struct Coordinate {
-   x: i32,
-   y: i32
+   x: i64,
+   y: i64
 }
 
 #[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
@@ -25,7 +25,7 @@ pub fn read(input: &str) -> Vec<Machine> {
     let mut machines = vec![];
 
     for captures in RE.captures_iter(input) {
-        let numbers: Vec<i32> = captures.iter()
+        let numbers: Vec<i64> = captures.iter()
             .skip(1)
             .filter_map(|m| m.map(|m| m.as_str().parse().unwrap()))
             .collect();
@@ -34,17 +34,18 @@ pub fn read(input: &str) -> Vec<Machine> {
         machines.push(Machine{
             a: Coordinate { x: numbers[0] ,y:numbers[1] },
             b: Coordinate { x: numbers[2] ,y:numbers[3] },
-            p: Coordinate { x: numbers[4] ,y:numbers[5] }
+            p: Coordinate { x: numbers[4] + 10000000000000 ,y:numbers[5] + 10000000000000 }
         })
     }
     machines
 }
 
 fn is_integer(f: f64) -> bool {
-    f64::abs(f - f.round()) < 1e-6
+    println!("is integer {f} {}: {}", f.round(), f64::abs(f - f.round()) < 0.1);
+    f64::abs(f - f.round()) < 1e-3
 }
 
-pub fn minimum_cost(machine: &Machine) -> i32 {
+pub fn minimum_cost(machine: &Machine) -> i64 {
     let xa = machine.a.x as f64;
     let ya = machine.a.y as f64;
     let xb = machine.b.x as f64;
@@ -55,14 +56,14 @@ pub fn minimum_cost(machine: &Machine) -> i32 {
     let na = (yp/yb - xp/xb) / (ya/yb - xa/xb);
     println!("{na}, {nb}");
     if is_integer(nb) && is_integer(na) {
-        na.round() as i32 * 3 + nb.round() as i32
+        na.round() as i64 * 3 + nb.round() as i64
     }
     else {
         0
     }
 }
 
-pub fn total_cost(machines: Vec<Machine>) -> i32 {
+pub fn total_cost(machines: Vec<Machine>) -> i64 {
     machines.iter().map(|m| minimum_cost(m)).sum()
 }
 
@@ -71,8 +72,13 @@ mod tests {
     use super::*;
 
     #[test]
+    fn it_is_integer() {
+        assert_eq!(is_integer(132643157579.00002), true);
+    }
+
+    #[test]
     fn it_works() {
-        assert_eq!(total_cost(read(inputs::EXAMPLE)), 480);
-        assert_eq!(total_cost(read(inputs::INPUT)), 40369);
+        assert_eq!(total_cost(read(inputs::EXAMPLE)), 875318608908);
+        assert_eq!(total_cost(read(inputs::INPUT)), 72587986598368);
     }
 }
