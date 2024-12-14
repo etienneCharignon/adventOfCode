7dafbc9fbe86ed91478932e8dbcd74c4da2bd382
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-14 09:48:35 +0100

    j14 partie 1

---
+++ 2024/j14/src/lib.rs
@@ -0,0 +1,115 @@
+mod inputs;
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"([-0-9]+)").unwrap();
+}
+
+#[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
+pub struct Vector {
+   x: i64,
+   y: i64
+}
+
+#[derive(Debug, PartialEq, Copy, Clone, Hash, Eq)]
+pub struct Robot {
+   p: Vector,
+   v: Vector
+}
+
+pub fn read(input: &str) -> Vec<Robot> {
+    let mut robots = vec![];
+
+    let mut matches = RE.find_iter(input)
+        .map(|m| m.as_str().parse::<i64>().unwrap());
+
+    while let (Some(px), Some(py), Some(vx), Some(vy)) = (
+        matches.next(), 
+        matches.next(), 
+        matches.next(), 
+        matches.next()
+    ) {
+        // println!("Position: ({}, {}), Velocity: ({}, {})", px, py, vx, vy);
+
+        robots.push(Robot{
+            p: Vector { x: px, y: py },
+            v: Vector { x: vx, y: vy }
+        })
+    }
+    robots
+}
+
+pub fn move_robots(robots: Vec<Robot>, width: i64, height: i64, n: i64) -> Vec<Robot> {
+    let mut new_pos = vec![];
+    for robot in robots {
+        new_pos.push(move_robot(robot, width, height, n));
+    }
+    new_pos
+}
+
+pub fn safety_factor(robots: Vec<Robot>, width: i64, height: i64, n: i64) -> i64 {
+    let last_positions = move_robots(robots, width, height, n);
+    let mut quadrants = [0; 4];
+    for robot in last_positions {
+        if robot.p.x < width / 2 {
+            if robot.p.y < height / 2 {
+                quadrants[0] += 1;
+            }
+            else if robot.p.y > height / 2{
+                quadrants[1] += 1
+            }
+        }
+        else if robot.p.x > width / 2 {
+            if robot.p.y < height / 2 {
+                quadrants[2] += 1;
+            }
+            else if robot.p.y > height / 2{
+                quadrants[3] += 1
+            }
+        }
+    } 
+    println!("{:?}",  quadrants);
+    quadrants.iter().product()
+}
+
+pub fn move_robot(robot: Robot, width: i64, height: i64, n: i64) -> Robot {
+    let mut new_robot = Robot {
+        p: Vector {
+            x: (robot.p.x + n * robot.v.x) % width,
+            y: (robot.p.y + n * robot.v.y) % height,
+        },
+        v: robot.v
+    };
+    if new_robot.p.x < 0 {
+        new_robot.p.x += new_robot.p.x / width + width;
+    }
+    if new_robot.p.y < 0 {
+        new_robot.p.y += new_robot.p.y / height + height;
+    }
+    new_robot
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_move_robot() {
+        assert_eq!(move_robot(Robot { p: Vector { x: 2, y: 6 }, v: Vector { x: 2, y: -3 } }, 11, 7, 1),
+                              Robot { p: Vector { x: 4, y: 3 }, v: Vector { x: 2, y: -3 } });
+        assert_eq!(move_robot(Robot { p: Vector { x: 10, y: 6 }, v: Vector { x: 1, y: 1 } }, 11, 7, 1),
+                              Robot { p: Vector { x: 0, y: 0 }, v: Vector { x: 1, y: 1 } });
+        assert_eq!(move_robot(Robot { p: Vector { x: 0, y: 0 }, v: Vector { x: -2, y: -2 } }, 11, 7, 1),
+                              Robot { p: Vector { x: 9, y: 5 }, v: Vector { x: -2, y: -2 } });
+        assert_eq!(move_robot(Robot { p: Vector { x: 0, y: 0 }, v: Vector { x: -1, y: -1 } }, 11, 7, 1),
+                              Robot { p: Vector { x: 10, y: 6 }, v: Vector { x: -1, y: -1 } });
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(safety_factor(read(inputs::EXAMPLE), 11, 7, 100), 12);
+        assert_eq!(safety_factor(read(inputs::INPUT), 101, 103, 100), 236628054);
+    }
+}
