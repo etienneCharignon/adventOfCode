4b1835963e5fc6bffa6c58fe91eaa65d7a79ae3e
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-17 21:05:25 +0100

    j17/src/lib.rs

--- 2024/j17/src/lib.rs
+++ 2024/j17/src/lib.rs
@@ -1,6 +1,6 @@
 mod inputs;
 
-pub fn combo(operand: u32, registers: &Vec<u32>) -> u32 {
+pub fn combo(operand: u64, registers: &Vec<u64>) -> u64 {
     match operand {
         0|1|2|3 => operand,
         4|5|6 => registers[operand as usize - 4],
@@ -8,9 +8,9 @@ pub fn combo(operand: u32, registers: &Vec<u32>) -> u32 {
     }
 }
 
-pub fn run(initial_registers: &Vec<u32>, program: &Vec<u32>) -> Vec<u32> {
+pub fn run(initial_registers: &Vec<u64>, program: &Vec<u64>) -> Vec<u64> {
     let mut registers = initial_registers.clone();
-    let mut output: Vec<u32> = vec![];
+    let mut output: Vec<u64> = vec![];
     let mut ip: usize = 0;
     loop {
         if ip >= program.len() {
@@ -21,7 +21,7 @@ pub fn run(initial_registers: &Vec<u32>, program: &Vec<u32>) -> Vec<u32> {
         ip += 2;
         match opcode {
             0 => { // adv
-                registers[0] = registers[0] / 2_u32.pow(combo(operand, &registers));
+                registers[0] = registers[0] / 2_u64.pow(combo(operand, &registers) as u32);
             },
             1 => { // bxl
                 registers[1] ^= operand;
@@ -41,15 +41,64 @@ pub fn run(initial_registers: &Vec<u32>, program: &Vec<u32>) -> Vec<u32> {
                 output.push(combo(operand, &registers) % 8);
             },
             6 => { // bdv
-                registers[1] = registers[0] / 2_u32.pow(combo(operand, &registers));
+                registers[1] = registers[0] / 2_u64.pow(combo(operand, &registers) as u32);
             },
             7 => { // cdv
-                registers[2] = registers[0] / 2_u32.pow(combo(operand, &registers));
+                registers[2] = registers[0] / 2_u64.pow(combo(operand, &registers) as u32);
             },
             _ => { panic!("unknow opcode {}", opcode)}
         }
     }
     output
+}
+
+pub fn rprogram(a_initial: u64) -> Vec<u64> {
+    let mut a: u64 = a_initial;
+    let mut output: Vec<u64> = vec![];
+    while a > 0 {
+        let mut b = (a % 8) ^ 1;
+        let c = (a >> b) % 8;
+        b = b ^ 4 ^ c;
+        output.push(b);
+
+        a = a >> 3;
+    }
+    output
+}
+
+pub fn solve(program: &Vec<u64>, solution: u64, i: usize) -> Option<u64> {
+    if i >= program.len() {
+        return Some(solution);
+    }
+    if i == 0 {
+        for a in 1..=1023 {
+            let output = rprogram(a);
+            if output.len() > 0 && output[0] == program[i] {
+                println!("0 {a:o} : {:?}", output);
+                match solve(program, a>>3, i + 1) {
+                    Some(res) => { return Some((res << 3) + (a % 8)); }
+                    None => {}
+                }
+            }
+        }
+    } else {
+        for highbits in 0..=7 {
+            let a = solution | (highbits << 7);
+            let output = rprogram(a);
+            if output.len() > 0 && output[0] == program[i] && (output.len() + i <= program.len()) {
+                println!("{i} {a:o} : {:?} ({:?})", output, output.len() > 0 && output[0] == program[i] );
+                match solve(program, a>>3, i + 1) {
+                    Some(res) => {
+                        let found = (res << 3) + (a % 8);
+                        println!("found {found:o}");
+                        return Some(found);
+                    }
+                    None => {}
+                }
+            }
+        }
+    }
+    None
 }
 
 #[cfg(test)]
@@ -94,8 +143,22 @@ mod tests {
     }
 
     #[test]
-    fn it_works() {
+    fn it_works_part1() {
         assert_eq!(run(&inputs::EXAMPLE_REGISTERS, &inputs::EXAMPLE_PROGRAM), vec![4,6,3,5,6,3,5,2,1,0]);
         assert_eq!(run(&inputs::INPUT_REGISTERS,&inputs::INPUT_PROGRAM),vec![7,1,2,3,2,6,7,2,5]);
+    }
+
+    #[test]
+    fn it_works_part2() {
+        assert_eq!(solve(&inputs::INPUT_PROGRAM, 0, 0), Some(202356708354602));
+    }
+
+    #[test]
+    fn it_simulates() {
+        for a in 1..2000 {
+            let output = run(&vec![a,0,0], &inputs::INPUT_PROGRAM);
+            let output2 = rprogram(a);
+            assert_eq!(output2, output, "{a}");
+        }
     }
 }
