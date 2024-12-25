db570757d7569a0ae379ba396fb71140e115cfe4
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-25 19:09:15 +0100

    optimisation

--- 2024/j24/src/lib.rs
+++ 2024/j24/src/lib.rs
@@ -9,15 +9,15 @@ lazy_static! {
     static ref RE: Regex = Regex::new(r"(.{3}) (.{2,3}) (.{3}) -> (.{3})").unwrap();
 }
 
-
 #[derive(Debug, PartialEq, Clone, Hash, Eq)]
 pub struct Gate {
    input1: String,
    input2: String,
-   operator: String
+   operator: String,
+   operate: fn(u64, u64) -> u64,
 }
 
-pub fn read_wires(input: &str) -> HashMap<String, i64> {
+pub fn read_wires(input: &str) -> HashMap<String, u64> {
     let mut wires = HashMap::new();
     for wire in input.split('\n') {
         let parts: Vec<&str> = wire.split(": ").collect();
@@ -29,9 +29,16 @@ pub fn read_wires(input: &str) -> HashMap<String, i64> {
 pub fn read_gates(input: &str) -> HashMap<String, Gate> {
     let mut gates = HashMap::new();
     for groups in RE.captures_iter(input) {
+        let operator_string: &str = &groups[2];
         let gate = Gate {
             input1: groups[1].to_string(),
-            operator: groups[2].to_string(),
+            operator: operator_string.to_string(),
+            operate: match operator_string {
+                "OR" => |input1, input2| input1 | input2,
+                "AND" => |input1, input2| input1 & input2,
+                "XOR" => |input1, input2| input1 ^ input2,
+                _ => panic!("Unknown operator"),
+            },
             input2: groups[3].to_string()
         };
         gates.insert(groups[4].to_string(), gate);
@@ -39,27 +46,22 @@ pub fn read_gates(input: &str) -> HashMap<String, Gate> {
     gates
 }
 
-pub fn read(wire: String, gates: &HashMap<String, Gate>, states: &HashMap<String, i64>) -> i64 {
-    if let Some(&value) = states.get(&wire) {
+pub fn read(wire: &String, gates: &HashMap<String, Gate>, states: &HashMap<String, u64>) -> u64 {
+    if let Some(&value) = states.get(wire) {
         return value;
     }
-    match gates.get(&wire) {
+    match gates.get(wire) {
         Some(gate) => {
             println!("{wire} : {gate:?}");
-            let input1 = read(gate.input1.clone(), gates, states);
-            let input2 = read(gate.input2.clone(), gates, states);
-            match gate.operator.as_str() {
-                "OR" => input1 | input2,
-                "AND" => input1 & input2,
-                "XOR" => input1 ^ input2,
-                _ => panic!("unknown operator {}", gate.operator)
-            }
+            let input1 = read(&gate.input1, gates, states);
+            let input2 = read(&gate.input2, gates, states);
+            (gate.operate)(input1, input2)
         },
         _ => panic!("wire unknown {wire}")
     }
 }
 
-pub fn read_zs(initial_sates: HashMap<String, i64>, gates: HashMap<String, Gate>) -> i64 {
+pub fn read_zs(initial_sates: HashMap<String, u64>, gates: HashMap<String, Gate>) -> u64 {
     let mut zs = 0;
     let mut digit = 0;
     loop {
@@ -67,8 +69,8 @@ pub fn read_zs(initial_sates: HashMap<String, i64>, gates: HashMap<String, Gate>
         if ! gates.contains_key(&z_id) {
             break;
         }
-        let z_value = read(z_id, &gates, &initial_sates);
-        zs += z_value * 2_i64.pow(digit);
+        let z_value = read(&z_id, &gates, &initial_sates);
+        zs += z_value * 2_u64.pow(digit);
         digit += 1;
     }
     zs
@@ -81,6 +83,6 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(read_zs(read_wires(inputs::EXAMPLE_INTIAL_STATE), read_gates(inputs::EXAMPLE_GATES)), 2024);
-        assert_eq!(read_zs(read_wires(inputs::INPUT_INTIAL_STATE), read_gates(inputs::INPUT_GATES)), 2024);
+        assert_eq!(read_zs(read_wires(inputs::INPUT_INTIAL_STATE), read_gates(inputs::INPUT_GATES)), 53755311654662);
     }
 }
