30b683768eeb4764f50c561c02ed684be5c8c50b
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-25 17:15:25 +0100

    j23 partie 2

--- 2024/j23/src/lib.rs
+++ 2024/j23/src/lib.rs
@@ -13,14 +13,19 @@ pub fn insert_pair(computer1: String, computer2:String, pairs: &mut HashMap<Stri
     }
 }
 
-pub fn count_t_computers(input: &str) -> usize {
+pub fn read_pairs(input: &str) -> HashMap<String, HashSet<String>> {
     let mut pairs = HashMap::<String, HashSet<String>>::new();
     for pair in input.split('\n') {
         let computers: Vec<&str> = pair.split('-').collect();
         insert_pair(computers[0].to_string(), computers[1].to_string(), &mut pairs);
         insert_pair(computers[1].to_string(), computers[0].to_string(), &mut pairs);
     }
-    println!("{pairs:?}");
+    // println!("{pairs:?}");
+    pairs
+}
+
+pub fn count_t_computers(input: &str) -> usize {
+    let pairs = read_pairs(input);
     let mut clusters: HashSet<Vec<&str>> = HashSet::new();
     for pair in input.split('\n') {
         let computers: Vec<&str> = pair.split('-').collect();
@@ -37,13 +42,50 @@ pub fn count_t_computers(input: &str) -> usize {
     clusters.iter().filter(|cluster| cluster.iter().any(|c| c.starts_with('t'))).count()
 }
 
+pub fn find_largest_cluster(input:&str) -> String {
+    let pairs = read_pairs(input);
+    let mut clusters = HashSet::<Vec<&str>>::new();
+    for (computer, connexions) in pairs.iter() {
+        let mut found = false;
+        clusters = clusters.into_iter().map(|cluster| {
+            if cluster.iter().all(|computer| connexions.contains(*computer)) {
+                found = true;
+                let mut new_cluster = cluster;
+                new_cluster.push(computer);
+                new_cluster.sort();
+                new_cluster
+            }
+            else {
+                cluster
+            }
+        }).collect();
+        if ! found {
+            clusters.insert(vec![&computer]);
+        }
+    }
+    println!("\n{clusters:?}");
+    let mut largest = vec![];
+    for cluster in clusters {
+        if cluster.len() > largest.len() {
+            largest = cluster;
+        }
+    }
+    largest.join(",")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works_p1() {
         assert_eq!(count_t_computers(inputs::EXAMPLE), 7);
         assert_eq!(count_t_computers(inputs::INPUT), 1077);
+    }
+
+    #[test]
+    fn it_works_p2() {
+        assert_eq!(find_largest_cluster(inputs::EXAMPLE), "co,de,ka,ta");
+        assert_eq!(find_largest_cluster(inputs::INPUT), "bc,bf,do,dw,dx,ll,ol,qd,sc,ua,xc,yu,zt");
     }
 }
