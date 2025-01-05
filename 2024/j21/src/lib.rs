9b214de3c0438dd96d460a27b2fed70d0461ee9a
Étienne Charignon <etienne.charignon@gmail.com>
2025-01-05 19:58:37 +0100

    j21 partie 2

--- 2024/j21/src/lib.rs
+++ 2024/j21/src/lib.rs
@@ -2,8 +2,8 @@ mod inputs;
 
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::cmp::min;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 lazy_static! {
     static ref RE: Regex = Regex::new(r"([0-9]+)").unwrap();
@@ -33,6 +33,7 @@ pub fn numeric_pos(key: char) -> Pos {
         '3' => Pos {x: 2, y: 2},
         '0' => Pos {x: 1, y: 3},
         'A' => Pos {x: 2, y: 3},
+        ' ' => Pos {x: 0, y: 3},
         _ => panic!("Unknown position")
     }
 }
@@ -44,6 +45,7 @@ pub fn directionnal_pos(key: char) -> Pos {
         '<' => Pos {x: 0, y: 1},
         'v' => Pos {x: 1, y: 1},
         '>' => Pos {x: 2, y: 1},
+        ' ' => Pos {x: 0, y: 0},
         _ => panic!("Unknown position")
     }
 }
@@ -95,100 +97,150 @@ pub fn move_to_key(from: Pos, to:Pos, h_first: bool, blank: Pos) -> String {
     sequence
 }
 
-pub fn append_char_sequences(sequences: Vec<String>, char_sequences: HashSet<String>) -> Vec<String> {
-    let mut new_sequences = vec![];
-    for sequence in sequences {
-        for char_sequence in &char_sequences {
-            new_sequences.push(sequence.clone() + &char_sequence);
-        }
-    }
-    new_sequences
+pub fn append_char_sequences(sequences: HashSet<String>, char_sequences: HashSet<String>) -> HashSet<String> {
+    sequences
+        .iter()
+        .flat_map(|sequence| char_sequences.iter().map(move |char_sequence| sequence.to_owned() + &char_sequence))
+        .collect()
+    // let mut new_sequences = HashSet::new();
+    // for sequence in sequences {
+    //     for char_sequence in &char_sequences {
+    //         new_sequences.insert(format!("{sequence}{}", char_sequence));
+    //     }
+    // }
+    // new_sequences
 }
 
-pub fn sequence_to_pad(to_pos: fn(char) -> Pos, blank: Pos, codes: HashSet<String>) -> HashSet<String> {
-    let mut all_sequences = HashSet::new();
+pub fn sequence_to_pad(to_pos: fn(char) -> Pos, blank: Pos, codes: HashSet<String>, robot: usize,
+                       cache: &mut HashMap<(usize, String), HashSet<String>>) -> HashSet<String> {
+    let mut all_sequences: HashSet<String> = HashSet::new();
     let mut current: char = 'A';
     for code in codes {
-        let mut sequences = vec![];
-        sequences.push(String::new());
+        let key = (robot, code.clone());
+        if cache.contains_key(&key) {
+            for sequence in cache.get(&key).unwrap() {
+                all_sequences.insert(sequence.clone());
+            }
+            continue;
+        }
+
+        let mut sequences = HashSet::new();
+        sequences.insert(String::new());
         for dest in code.chars() {
             let mut all_sequences_for_char = HashSet::new();
             for priority in [true, false] {
                 let mut s = String::new();
                 s += &move_to_key(to_pos(current), to_pos(dest), priority, blank);
-                s += "A";
+                s.push('A');
                 all_sequences_for_char.insert(s);
             }
             sequences = append_char_sequences(sequences, all_sequences_for_char);
             current = dest;
         }
-        for sequence in sequences {
+        for sequence in sequences.clone() {
             all_sequences.insert(sequence);
         }
+        cache.insert((robot, code), sequences);
     }
     all_sequences
 }
 
 pub fn unitary_move(from: char, to: char, to_pos: fn(char) -> Pos, blank: Pos) -> HashSet<String> {
-    let mut shortest = HashSet::new();
-    let mut min_len = usize::MAX;
-
-    let mut sequences = vec![];
-    sequences.push(String::new());
-    let mut shortest_for_char = HashSet::new();
+    let mut sequences = HashSet::new();
     for priority in [true, false] {
         let mut s = String::new();
         s += &move_to_key(to_pos(from), to_pos(to), priority, blank);
         s += "A";
-        shortest_for_char.insert(s);
+        sequences.insert(s);
     }
-    sequences = append_char_sequences(sequences, shortest_for_char);
-
-    for sequence in sequences {
-        min_len = min(min_len, sequence.len());
-        shortest.insert(sequence);
-    }
-
-    // shortest.into_iter().filter(|sequence| sequence.len() == min_len).collect()
-    shortest
+    sequences
 }
 
-pub fn final_sequence(code: &str) -> String {
-    let all_sequences = sequence_to_pad(directionnal_pos, Pos {x: 0, y: 0},
-        sequence_to_pad(directionnal_pos, Pos {x: 0, y: 0},
-            sequence_to_pad(numeric_pos, Pos {x: 0, y: 3}, to_set(vec![code]))));
+pub fn final_sequence(code: &str) -> usize {
+    let mut sequences = sequence_to_pad(numeric_pos, Pos {x: 0, y: 3}, to_set(vec![code]), 0, &mut HashMap::new());
+    println!("nombre sequence : {}", sequences.len());
+    let directional_blank = Pos {x: 0, y: 0};
+    let mut cache = HashMap::new();
+    for i in 0..2 {
+        sequences = sequence_to_pad(directionnal_pos, directional_blank, sequences, i + 1, &mut cache);
+        println!("nombre sequence : {}", sequences.len());
+    }
+    println!("finit");
     // println!("{all_sequences:?}");
-    all_sequences.iter()
+    sequences.iter()
         .fold(String::from(""),
               |shortest, sequence|
               if shortest.len() > 0 && shortest.len() <= sequence.len() {
                   shortest
               } else {
                   sequence.clone()
-              })
+              }).len()
+}
+ pub fn minimum_len(code: String, irobot:usize, nbr_robot: usize, cache: &mut HashMap<(usize, char, char), usize>) -> usize {
+    let mut sequence_len = 0;
+    let mut chars: Vec<char> = code.chars().collect();
+    chars.insert(0, 'A');
+    for i in 0..chars.len()-1 {
+        sequence_len += minimum_for_a_char(chars[i], chars[i+1], irobot, nbr_robot, cache);
+    }
+    sequence_len
+ }
 
-//     let digits: Vec<char> = format!("A{code}").chars().collect();
-//     let mut result = String::new();
-//     for i in 0..code.len() {
-//         result += sequence_to_pad(directionnal_pos, Pos {x: 0, y: 0},
-//             sequence_to_pad(directionnal_pos, Pos {x: 0, y: 0},
-//                 unitary_move(digits[i], digits[i+1], numeric_pos, Pos {x: 0, y: 3})))
-//             .iter()
-//             .fold(&String::from("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"),
-//                  |shortest, sequence| if shortest.len() <= sequence.len() { shortest } else { sequence });
-//         }
-//     result
+pub fn minimum_for_a_char(from: char, to:char, irobot:usize, nbr_robot: usize, cache: &mut HashMap<(usize, char, char), usize>) -> usize {
+    let key = (irobot, from, to);
+    if cache.contains_key(&key) {
+        return *cache.get(&key).unwrap();
+    }
+
+    let mut sequences = HashSet::new();
+    if irobot == nbr_robot {
+        for priority in [true, false] {
+            let mut s = String::new();
+            s += &move_to_key(numeric_pos(from), numeric_pos(to), priority, numeric_pos(' '));
+            s.push('A');
+            sequences.insert(minimum_len(s, irobot - 1, nbr_robot, cache));
+        }
+    }
+    else {
+        for priority in [true, false] {
+            let mut s = String::new();
+            s += &move_to_key(directionnal_pos(from), directionnal_pos(to), priority, directionnal_pos(' '));
+            s.push('A');
+            if irobot > 0 {
+                sequences.insert(minimum_len(s, irobot - 1, nbr_robot, cache));
+            }
+            else {
+                sequences.insert(s.len());
+            }
+        }
+    }
+    let min = *sequences.iter().min().unwrap();
+    cache.insert(key, min);
+    min
 }
 
 pub fn complexity(code: &str) -> usize {
-    let sequence = final_sequence(code);
-    println!("{code}: {}, {sequence}", sequence.len());
+    let sequence_len = final_sequence(code);
+    println!("{code}: {}, {sequence_len}", sequence_len);
 
-    sequence.len() * RE.find(code).unwrap().as_str().parse::<usize>().unwrap()
+    sequence_len * RE.find(code).unwrap().as_str().parse::<usize>().unwrap()
+}
+
+pub fn complexity2(code: &str) -> usize {
+    let mut cache = HashMap::new();
+    let number_of_robot = 25;
+    let sequence_len = minimum_len(code.to_string(), number_of_robot, number_of_robot, &mut cache);
+    println!("{code}: {}, {sequence_len}", sequence_len);
+
+    sequence_len * RE.find(code).unwrap().as_str().parse::<usize>().unwrap()
 }
 
 pub fn complexities(input: &str) -> usize {
     input.split("\n").map(|code| { complexity(code) }).sum()
+}
+
+pub fn complexities2(input: &str) -> usize {
+    input.split("\n").map(|code| { complexity2(code) }).sum()
 }
 
 // example 029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
@@ -237,11 +289,13 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn it_compute_sequence_to_numeric_key_pad() {
-        assert_eq!(sequence_to_pad(numeric_pos, Pos { x: 0, y: 3 }, to_set(vec!["029A"])),
+        assert_eq!(sequence_to_pad(numeric_pos, Pos { x: 0, y: 3 }, to_set(vec!["029A"]), 0, &mut HashMap::new()),
                    to_set(vec!["<A^A>^^AvvvA", "<A^A^^>AvvvA"]));
+        let mut cache = HashMap::new();
         assert_eq!(sequence_to_pad(directionnal_pos, Pos {x: 0, y: 0},
-                sequence_to_pad(numeric_pos, Pos { x: 0, y: 3 }, to_set(vec!["029A"]))),
+                sequence_to_pad(numeric_pos, Pos { x: 0, y: 3 }, to_set(vec!["029A"]), 0, &mut cache), 1, &mut cache),
                    to_set(vec![
                        "v<<A>>^A<A>A<AAv>A^Av<AAA^>A",
                        "v<<A>>^A<A>AvA<^AA>Av<AAA^>A",
@@ -262,13 +316,14 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn it_compute_final_sequence() {
         assert_eq!((
-                final_sequence("029A").len(),
-                final_sequence("980A").len(),
-                final_sequence("179A").len(),
-                final_sequence("456A").len(),
-                final_sequence("379A").len(),
+                final_sequence("029A"),
+                final_sequence("980A"),
+                final_sequence("179A"),
+                final_sequence("456A"),
+                final_sequence("379A"),
                 ),
                 (
                     "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A".len(),
@@ -281,8 +336,14 @@ mod tests {
     }
 
     #[test]
-    fn it_works() {
+    #[ignore]
+    fn it_works_p1() {
         assert_eq!(complexities(inputs::EXAMPLE), 126384);
         assert_eq!(complexities(inputs::INPUT), 107934);
+    }
+
+    #[test]
+    fn it_works_p2() {
+        assert_eq!(complexities2(inputs::INPUT), 130470079151124);
     }
 }
