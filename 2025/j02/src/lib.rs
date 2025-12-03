a9ee4dc1f41f76652dbd7cd6134b40a9bbb7b173
Étienne Charignon <etienne.charignon@gmail.com>
2025-12-03 22:34:22 +0100

    optimise le jour 2

--- 2025/j02/src/lib.rs
+++ 2025/j02/src/lib.rs
@@ -2,26 +2,21 @@ mod inputs;
 
 pub fn invalide(n: &str, multiple: usize) -> bool {
     println!("{multiple}");
-    let chars: Vec<_> = n.chars().collect();
-    if chars.len() % multiple != 0 {
+    if !n.len().is_multiple_of(multiple) {
         return false;
     }
-    let sections: Vec<&str> = n
-        .as_bytes()
-        .chunks(chars.len() / multiple)
+    let taille = n.len() / multiple;
+    n.as_bytes()
+        .chunks(taille)
         .map(|chunk| std::str::from_utf8(chunk).unwrap())
-        .collect();
-    let premier = sections[0];
-    sections.iter().all(|s| *s == premier)
+        .all(|s| *s == n[..taille])
 }
 
-pub fn trouve_invalide(interval_str: &str) -> Vec<i64> {
+pub fn trouve_invalides(interval_str: &str) -> Vec<i64> {
     let interval: Vec<i64> = interval_str
         .split('-')
-        .collect::<Vec<_>>()
-        .iter()
         .map(|s| s.parse::<i64>().unwrap())
-        .collect::<Vec<_>>();
+        .collect();
     let mut invalides: Vec<i64> = vec![];
     for i in interval[0]..=interval[1] {
         let istr = i.to_string();
@@ -38,9 +33,7 @@ pub fn trouve_invalide(interval_str: &str) -> Vec<i64> {
 pub fn somme(inputs: &str) -> i64 {
     inputs
         .split(',')
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|interval| trouve_invalide(interval).iter().sum::<i64>())
+        .map(|interval| trouve_invalides(interval).iter().sum::<i64>())
         .sum()
 }
 
@@ -50,9 +43,9 @@ mod tests {
 
     #[test]
     fn il_trouve_invalide_dans_un_interval() {
-        assert_eq!(trouve_invalide("11-22"), vec![11, 22]);
-        assert_eq!(trouve_invalide("95-115"), vec![99, 111]);
-        assert_eq!(trouve_invalide("1188511880-1188511890"), vec![1188511885]);
+        assert_eq!(trouve_invalides("11-22"), vec![11, 22]);
+        assert_eq!(trouve_invalides("95-115"), vec![99, 111]);
+        assert_eq!(trouve_invalides("1188511880-1188511890"), vec![1188511885]);
     }
 
     #[test]
