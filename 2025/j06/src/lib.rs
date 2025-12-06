50df44a6b85c7fd0901a44406770c5fbece0c6d7
Étienne Charignon <etienne.charignon@gmail.com>
2025-12-06 10:05:41 +0100

    jour 6 partie 2

--- 2025/j06/src/lib.rs
+++ 2025/j06/src/lib.rs
@@ -1,6 +1,6 @@
 mod inputs;
 
-pub fn somme_problèmes(cahier: &str) -> i64 {
+pub fn somme_problèmes_1(cahier: &str) -> i64 {
     let lignes: Vec<_> = cahier.lines().collect();
     let opérations: Vec<_> = lignes[lignes.len() - 1].split_whitespace().collect();
     let nombres: Vec<_> = lignes[..lignes.len() - 1]
@@ -34,13 +34,64 @@ pub fn somme_problèmes(cahier: &str) -> i64 {
         .sum()
 }
 
+pub fn somme_problèmes_2(cahier: &str) -> i64 {
+    let lignes: Vec<_> = cahier.lines().collect();
+    let opérations: Vec<_> = lignes[lignes.len() - 1].split_whitespace().collect();
+    let acc = lignes[0].chars().map(|_| String::new()).collect();
+    let chaines: Vec<_> = lignes[..lignes.len() - 1]
+        .iter()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .fold(acc, |acc, chars| {
+            acc.iter()
+                .enumerate()
+                .map(|(i, colonne)| {
+                    let mut nouvelle_chaine = colonne.clone();
+                    nouvelle_chaine.push(chars[i]);
+                    nouvelle_chaine
+                })
+                .collect()
+        });
+    println!("{opérations:?}, {chaines:?}");
+
+    let mut total = 0;
+    let mut index_opération = 0;
+    let mut resultat = match opérations[index_opération] {
+        "*" => 1,
+        _ => 0,
+    };
+    for chaine in chaines {
+        if chaine.trim() == "" {
+            index_opération += 1;
+            total += resultat;
+            resultat = match opérations[index_opération] {
+                "*" => 1,
+                _ => 0,
+            };
+            continue;
+        }
+        let nombre = chaine.trim().parse::<i64>().unwrap();
+        match opérations[index_opération] {
+            "*" => resultat *= nombre,
+            _ => resultat += nombre,
+        }
+    }
+    total += resultat;
+    total
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn il_fait_les_maths() {
-        assert_eq!(somme_problèmes(inputs::EXEMPLE), 4277556);
-        assert_eq!(somme_problèmes(inputs::INPUT), 4076006202939);
+    fn il_fait_les_maths_partie1() {
+        assert_eq!(somme_problèmes_1(inputs::EXEMPLE), 4277556);
+        assert_eq!(somme_problèmes_1(inputs::INPUT), 4076006202939);
+    }
+
+    #[test]
+    fn il_fait_les_maths_partie2() {
+        assert_eq!(somme_problèmes_2(inputs::EXEMPLE), 3263827);
+        assert_eq!(somme_problèmes_2(inputs::INPUT), 7903168391557);
     }
 }
