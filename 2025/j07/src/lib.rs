635cf6eab03f54f687d4f79a7a657fb3872d9854
Étienne Charignon <etienne.charignon@gmail.com>
2025-12-07 15:16:17 +0100

    simplification, inutile de garder toutes les lignes

--- 2025/j07/src/lib.rs
+++ 2025/j07/src/lib.rs
@@ -44,49 +44,42 @@ pub fn compte_division(collecteur: &str) -> i64 {
 }
 
 pub fn compte_lignes_temporelles(collecteur: &str) -> i64 {
-    let mut compteurs_lignes_temporelles: Vec<Vec<i64>> = vec![];
-    for (i, ligne) in collecteur.lines().enumerate() {
-        let nouvelle_ligne = if compteurs_lignes_temporelles.is_empty() {
-            ligne
-                .chars()
-                .map(|c| if c == 'S' { 1 } else { 0 })
-                .collect()
-        } else {
-            let chars: Vec<_> = ligne.chars().collect();
-            chars
-                .clone()
-                .iter()
-                .enumerate()
-                .map(|(x, c)| match c {
-                    '^' => 0,
-                    _ => {
-                        let mut nombres_de_lignes_temporelles = 0;
-                        if x < chars.len() - 1
-                            && chars[x + 1] == '^'
-                            && compteurs_lignes_temporelles[i - 1][x + 1] > 0
-                        {
-                            nombres_de_lignes_temporelles =
-                                compteurs_lignes_temporelles[i - 1][x + 1];
+    collecteur
+        .lines()
+        .fold(vec![], |ancienne_ligne, ligne| {
+            if ancienne_ligne.is_empty() {
+                ligne
+                    .chars()
+                    .map(|c| if c == 'S' { 1 } else { 0 })
+                    .collect()
+            } else {
+                let chars: Vec<_> = ligne.chars().collect();
+                chars
+                    .clone()
+                    .iter()
+                    .enumerate()
+                    .map(|(x, c)| match c {
+                        '^' => 0,
+                        _ => {
+                            let mut nombres_de_lignes_temporelles = 0;
+                            if x < chars.len() - 1
+                                && chars[x + 1] == '^'
+                                && ancienne_ligne[x + 1] > 0
+                            {
+                                nombres_de_lignes_temporelles = ancienne_ligne[x + 1];
+                            }
+                            if x > 0 && chars[x - 1] == '^' && ancienne_ligne[x - 1] > 0 {
+                                nombres_de_lignes_temporelles += ancienne_ligne[x - 1];
+                            }
+                            if ancienne_ligne[x] > 0 {
+                                nombres_de_lignes_temporelles += ancienne_ligne[x];
+                            }
+                            nombres_de_lignes_temporelles
                         }
-                        if x > 0
-                            && chars[x - 1] == '^'
-                            && compteurs_lignes_temporelles[i - 1][x - 1] > 0
-                        {
-                            nombres_de_lignes_temporelles +=
-                                compteurs_lignes_temporelles[i - 1][x - 1];
-                        }
-                        if compteurs_lignes_temporelles[i - 1][x] > 0 {
-                            nombres_de_lignes_temporelles += compteurs_lignes_temporelles[i - 1][x];
-                        }
-                        nombres_de_lignes_temporelles
-                    }
-                })
-                .collect()
-        };
-        compteurs_lignes_temporelles.push(nouvelle_ligne);
-    }
-    println!("{compteurs_lignes_temporelles:?}");
-    compteurs_lignes_temporelles[compteurs_lignes_temporelles.len() - 1]
+                    })
+                    .collect()
+            }
+        })
         .iter()
         .sum()
 }
