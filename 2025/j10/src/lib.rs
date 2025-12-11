2f06b8e4363e53ee34a5fc37c8f81fdd70288916
Étienne Charignon <etienne.charignon@gmail.com>
2025-12-11 09:31:39 +0100

    jour 10 partie 2 (en cours)

--- 2025/j10/src/lib.rs
+++ 2025/j10/src/lib.rs
@@ -15,6 +15,19 @@ pub fn appuie(etat: &Vec<bool>, bouton: &[i64]) -> Vec<bool> {
         .collect()
 }
 
+pub fn appuie_joltage(etat: &Vec<i64>, bouton: &[i64]) -> Vec<i64> {
+    etat.iter()
+        .enumerate()
+        .map(|(i, etat)| {
+            if bouton.contains(&(i as i64)) {
+                etat + 1
+            } else {
+                *etat
+            }
+        })
+        .collect()
+}
+
 pub fn affiche(état: &Vec<bool>) {
     let str: String = état.iter().map(|b| if *b { '#' } else { '.' }).collect();
     println!("{str}");
@@ -77,11 +90,71 @@ pub fn compte_appuis(entrées: &str) -> i64 {
     entrées.lines().map(compte_appuis_machine).sum()
 }
 
+pub fn trouve_état_joltage(
+    état_initial: &Vec<i64>,
+    cible: &Vec<i64>,
+    boutons: &[Vec<i64>],
+    n: i64,
+    min: i64,
+) -> (i64, bool) {
+    if n >= min || état_initial.iter().enumerate().any(|(i, j)| j > &cible[i]) {
+        return (n, false);
+    }
+    if état_initial == cible {
+        println!("====> Succés : n: {n}");
+        (n, true)
+    } else {
+        let mut trouvé = false;
+        let mut nouveau_min = min;
+        for bouton in boutons {
+            let état = appuie_joltage(état_initial, bouton);
+            //println!("appuie {bouton:?}, {}", n + 1);
+            let (retour_min, succès) =
+                trouve_état_joltage(&état, cible, boutons, n + 1, nouveau_min);
+            if succès {
+                nouveau_min = retour_min;
+                trouvé = true;
+            }
+        }
+        (nouveau_min, trouvé)
+    }
+}
+
+pub fn compte_appuis_machine_joltage(machine: &str) -> i64 {
+    let jetons: Vec<_> = machine.split_whitespace().collect();
+    let boutons: Vec<_> = jetons[1..jetons.len() - 1]
+        .iter()
+        .map(|b| b.trim_matches(&['(', ')'][..]))
+        .map(|b| {
+            b.split(",")
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    let cible: Vec<_> = jetons[jetons.len() - 1]
+        .trim_matches(&['{', '}'][..])
+        .split(",")
+        .map(|n| n.parse::<i64>().unwrap())
+        .collect();
+    println!("Entrée : {boutons:?} {cible:?}");
+    let état_initial = vec![0; cible.len()];
+    let (min, succes) = trouve_état_joltage(&état_initial, &cible, &boutons, 0, 15);
+    if !succes {
+        panic!("min initial issufisant.")
+    }
+    min
+}
+
+pub fn compte_appuis_joltage(entrées: &str) -> i64 {
+    entrées.lines().map(compte_appuis_machine_joltage).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    #[ignore = "travaille sur la partie 2"]
     fn il_appuie() {
         assert_eq!(appuie(&vec![false], &[0]), vec![true]);
         assert_eq!(appuie(&vec![false, false], &[1]), vec![false, true]);
@@ -90,7 +163,8 @@ mod tests {
     }
 
     #[test]
-    fn il_compt_les_appuis() {
+    #[ignore = "travaille sur la partie 2"]
+    fn il_compt_les_appuis_pour_les_lampes() {
         assert_eq!(
             compte_appuis_machine("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"),
             2
@@ -101,5 +175,11 @@ mod tests {
         );
         assert_eq!(compte_appuis(inputs::EXEMPLE), 7);
         assert_eq!(compte_appuis(inputs::INPUT), 535);
+    }
+
+    #[test]
+    fn il_compt_les_appuis_pour_joltage() {
+        // assert_eq!(compte_appuis_joltage(inputs::EXEMPLE), 33);
+        assert_eq!(compte_appuis_joltage(inputs::INPUT), 33);
     }
 }
